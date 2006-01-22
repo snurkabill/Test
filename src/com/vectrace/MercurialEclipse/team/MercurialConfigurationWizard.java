8a6e388970901bd243b7de5b1a018ae09d238b16
zingo <zingo@b1.home.zingo.org>
2006-01-22 16:42:51 +0200

    Create reposetory wizard is very nice now, but the actual command is not yet issued.

--- src/com/vectrace/MercurialEclipse/team/MercurialConfigurationWizard.java
+++ src/com/vectrace/MercurialEclipse/team/MercurialConfigurationWizard.java
@@ -1,4 +1,13 @@
 /**
+ * Mercurial create Wizard
+ * 
+ * This wizard will dig up the project root and then 
+ * it lets you modify the directory, when you are done it will 
+ * create a mercurial repository
+ * 
+ * It will follow the dirictory chain to the bottom to se is
+ * there is a .hg directory someware, is so it will suggest that you use it
+ * instead of creating a new repository.
  * 
  */
 package com.vectrace.MercurialEclipse.team;
@@ -7,15 +16,11 @@ import java.io.File;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.jface.preference.DirectoryFieldEditor;
-import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
@@ -26,31 +31,35 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.team.ui.IConfigurationWizard;
 import org.eclipse.ui.IWorkbench;
 
-import com.vectrace.MercurialEclipse.preferences.PreferenceConstants;
-
-
 /**
  * @author zingo
  *
  */
-public class MercurialConfigurationWizard extends Wizard implements IConfigurationWizard {
-
+public class MercurialConfigurationWizard extends Wizard implements IConfigurationWizard 
+{
 	public class NewWizardPage extends WizardPage implements SelectionListener 
 	{
-		String hgPath;
-		String hgPathOrginal;
-		Text directoryText;
 		Button changeDirButton;
 		Button restoreDefaultDirButton;
+		Button restoreExistingDirButton;
+		boolean newMercurialProject;
 		
-		NewWizardPage1(String hgPath) 
+		NewWizardPage(boolean newMercurialProject) 
 		{
 		    super( WizardPage.class.getName() );
-		    this.hgPath=hgPath;
-		    this.hgPathOrginal=hgPath;
-		    setTitle( "Mercurial Setup wizard" );
-		    setDescription( "Put this project under mercurial version control.\n Press Next to continue" );
-//		    String imgKey = "icons/sample.gif";
+		    this.newMercurialProject=newMercurialProject;
+		    if(newMercurialProject)
+		    {
+		    	setTitle( "Mercurial Setup wizard" );
+		    	setDescription( "Put this project under mercurial version control." );
+		    }
+		    else
+		    {
+		    	setTitle( "Mercurial Connect to existing repository" );
+		    	setDescription( "Connect to an existing mercurial version control" );
+		    }
+		    
+		    //		    String imgKey = "icons/sample.gif";
 //		    setImageDescriptor( new ImageDescriptor( ( imgKey ) );
 		    setPageComplete(true); /* Thel that it has the needed info */
 		  } 
@@ -71,26 +80,26 @@ public class MercurialConfigurationWizard extends Wizard implements IConfigurati
 
 		    directoryText = new Text(mainControl, SWT.CENTER | SWT.SINGLE);
 		    directoryText.setText(hgPath);
-
+		    directoryText.addSelectionListener(this);
+		    
 		    changeDirButton = new Button(mainControl, SWT.CENTER | SWT.PUSH);
 		    changeDirButton.setText("Change Dir");
 		    changeDirButton.addSelectionListener(this);
 
 			restoreDefaultDirButton = new Button(mainControl, SWT.CENTER | SWT.PUSH);
-			restoreDefaultDirButton.setText("Set Default");
+			restoreDefaultDirButton.setText("Use Project Root");
 			restoreDefaultDirButton.addSelectionListener(this);
 
+		    if(!newMercurialProject)
+		    {
+		    	restoreExistingDirButton = new Button(mainControl, SWT.CENTER | SWT.PUSH);
+		    	restoreExistingDirButton.setText("Use Existing .hg directory");
+		    	restoreExistingDirButton.addSelectionListener(this);
+		    }
 		    
 		    setControl(mainControl);
 		    setPageComplete(true);
-/*
-		    Label label = new Label( parent, SWT.NONE );
-		    label.setText( "Path:" + hgPath );
-		    setControl( label );
-*/
-//		    setControl(new DirectoryFieldEditor("first","&Directory preference:", parent));
-
-		  
+	  
 		  }
 
 		public void widgetSelected(SelectionEvent e) 
@@ -99,7 +108,6 @@ public class MercurialConfigurationWizard extends Wizard implements IConfigurati
 			{
 				// TODO Auto-generated method stub
 				DirectoryDialog directoryDialog;
-				System.out.println("widgetSelected()");
 			    directoryDialog = new DirectoryDialog( new Shell() );
 			    directoryDialog.setText("Select mercurial root");
 			    directoryDialog.setMessage("Select mercurial root, should be project root.");
@@ -113,44 +121,36 @@ public class MercurialConfigurationWizard extends Wizard implements IConfigurati
 				hgPath=hgPathOrginal;
 				directoryText.setText(hgPath);
 			}
+			else if(e.widget == directoryText)
+			{
+				hgPath=directoryText.getText();
+				directoryText.setText(hgPath);
+			}
+			else if((!newMercurialProject) && (e.widget == restoreExistingDirButton))
+			{
+				hgPath=foundhgPath;
+				directoryText.setText(hgPath);
+			}
 		}
 
 		public void widgetDefaultSelected(SelectionEvent e) 
 		{
-			// TODO Auto-generated method stub
-			System.out.println("widgetDefaultSelected()");
-			
+			//System.out.println("widgetDefaultSelected");
+			if(e.widget == directoryText)
+			{
+				hgPath=directoryText.getText();
+				directoryText.setText(hgPath);
+			}
+//      	This should not happend from a button widget.
 		}
 	}
 	
-	
-	public class ConnectWizardPage extends WizardPage {
-
-		String projPath;
-		String hgPath;
-		ConnectWizardPage(String projPath, String hgPath) {
-		    super( WizardPage.class.getName() );
-		    this.projPath=projPath;
-		    this.hgPath=hgPath;
-		    setTitle( "Mercurial Connect to existing repository" );
-		    setDescription( "Connect to an existing mercurial version control" );
-//		    String imgKey = "icons/sample.gif";
-//		    setImageDescriptor( new ImageDescriptor( ( imgKey ) );
-		  }
-		  
-		  
-		  // interface methods of CreateRepositoryPage
-		  ////////////////////////////////////////////
-		  
-		  public void createControl( final Composite parent ) {
-		    Label label = new Label( parent, SWT.NONE );
-		    label.setText( "projPath=" + projPath + "hgPath="+hgPath );
-		    setControl( label );
-		  }
-		}
-
-	
-	  private IProject project;
+	private IProject project;
+	private String hgPath;
+	private String hgPathOrginal;
+	private String foundhgPath;
+	private String pathProject;
+	private Text directoryText;
 	
 	
 	/**
@@ -158,45 +158,68 @@ public class MercurialConfigurationWizard extends Wizard implements IConfigurati
 	 */
 	public MercurialConfigurationWizard() {
 		//super();
-		// TODO Auto-generated constructor stub
 	    setWindowTitle( "MercurialConfigurationWizard" );
-		System.out.println("MercurialConfigurationWizard.MercurialConfigurationWizard()");
+//		System.out.println("MercurialConfigurationWizard.MercurialConfigurationWizard()");
 	}
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.jface.wizard.IWizard#addPages()
 	 */
 	public void addPages() {
-		// TODO Auto-generated method stub
-		System.out.println("MercurialConfigurationWizard.addPages()");
+//		System.out.println("MercurialConfigurationWizard.addPages()");
 
 	    IPath projectLocation = project.getLocation();
 	    String MercurialRootDir;
 	    MercurialRootDir=findMercurialRoot(projectLocation.toFile());
+	    pathProject=project.getLocation().toString();
 	    if (MercurialRootDir == null)
-	    {    
-			addPage( new NewWizardPage(project.getLocation().toString()) );
+	    {   
+	    	hgPathOrginal=project.getLocation().toString();
+	    	hgPath=hgPathOrginal;
+			addPage( new NewWizardPage(true) );
 	    }
 	    else
 	    {
-			addPage( new ConnectWizardPage(project.getLocation().toString(),MercurialRootDir ) );   	
+	    	foundhgPath=MercurialRootDir;
+	    	hgPathOrginal=MercurialRootDir;
+	    	hgPath=hgPathOrginal;
+			addPage( new NewWizardPage(false) );
 	    }
-	    
+	}
 
-    
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.wizard.IWizard#performFinish()
+	 */
+	public boolean performFinish() {
+		if(directoryText!=null)
+		{
+			hgPath=directoryText.getText();
+		}
+		// System.out.println("MercurialConfigurationWizard.preformFinish()");
+		System.out.println("execute >cd " + hgPath + ";hg init<");
+		return false;
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.team.ui.IConfigurationWizard#init(org.eclipse.ui.IWorkbench, org.eclipse.core.resources.IProject)
+	 */
+	public void init(IWorkbench workbench, IProject project) {
+		//System.out.println("MercurialConfigurationWizard.init()");
+		this.project=project;
 	}
 
     private static String findMercurialRoot( final File file ) {
         String path = null;
         File parent = file;
         File hgFolder = new File( parent, ".hg" );
-        System.out.println("pathcheck:" + parent.toString());
+//        System.out.println("pathcheck:" + parent.toString());
         while ((parent!=null) && !(hgFolder.exists() && hgFolder.isDirectory()) )
         {
         	parent=parent.getParentFile();
         	if(parent!=null)
         	{
-              System.out.println("pathcheck:" + parent.toString());
+//              System.out.println("pathcheck:" + parent.toString());
               hgFolder = new File( parent, ".hg" );
         	}
         }
@@ -208,29 +231,10 @@ public class MercurialConfigurationWizard extends Wizard implements IConfigurati
         {
           path = null;
         }
-        System.out.println("pathcheck: >" + path + "<");
+//        System.out.println("pathcheck: >" + path + "<");
         return path;
       }
-
 	
-
-	/* (non-Javadoc)
-	 * @see org.eclipse.jface.wizard.IWizard#performFinish()
-	 */
-	public boolean performFinish() {
-		// TODO Auto-generated method stub
-		System.out.println("MercurialConfigurationWizard.preformFinish()");
-		return false;
-	}
-	
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.team.ui.IConfigurationWizard#init(org.eclipse.ui.IWorkbench, org.eclipse.core.resources.IProject)
-	 */
-	public void init(IWorkbench workbench, IProject project) {
-		System.out.println("MercurialConfigurationWizard.init()");
-		this.project=project;
-	}
 
 	
 
