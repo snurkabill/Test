f1f9c0dc460ef072707cff993578d20053abf2ae
zingo <zingo@b1.home.zingo.org>
2006-04-20 23:14:10 +0200

    Zlib - License, Contrib file

---
+++ src/com/vectrace/MercurialEclipse/team/ActionDiff.java
@@ -0,0 +1,109 @@
+/**
+ * com.vectrace.MercurialEclipse (c) Vectrace Jan 31, 2006
+ * Created by zingo
+ */
+package com.vectrace.MercurialEclipse.team;
+
+import java.util.Iterator;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+//import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+
+/**
+ * @author zingo
+ *
+ */
+public class ActionDiff implements IWorkbenchWindowActionDelegate {
+
+//	private IWorkbenchWindow window;
+//    private IWorkbenchPart targetPart;
+    private IStructuredSelection selection;
+    
+	public ActionDiff() {
+		super();
+	}
+
+	/**
+	 * We can use this method to dispose of any system
+	 * resources we previously allocated.
+	 * @see IWorkbenchWindowActionDelegate#dispose
+	 */
+	public void dispose() {
+
+	}
+
+
+	/**
+	 * We will cache window object in order to
+	 * be able to provide parent shell for the message dialog.
+	 * @see IWorkbenchWindowActionDelegate#init
+	 */
+	public void init(IWorkbenchWindow window) {
+		System.out.println("ActionDiff:init(window)");
+//		this.window = window;
+	}
+
+	/**
+	 * The action has been activated. The argument of the
+	 * method represents the 'real' action sitting
+	 * in the workbench UI.
+	 * @see IWorkbenchWindowActionDelegate#run
+	 */
+	
+
+	public void run(IAction action) 
+	{
+		IProject proj;
+		String Repository;
+		String FullPath;
+		proj=MercurialUtilities.getProject(selection);
+		Repository=MercurialUtilities.getRepositoryPath(proj);
+		if(Repository==null)
+		{
+			Repository="."; //never leave this empty add a . to point to current path
+		}
+
+		Object obj;
+	    Iterator itr; 
+	    // the last argument will be replaced with a path
+		String launchCmd[] = { "hg","--cwd", Repository ,"diff", "" };
+	    itr=selection.iterator();
+	    while(itr.hasNext())
+	    {
+	    	obj=itr.next();
+	    	if (obj instanceof IResource)
+	    	{
+				//Setup and run command
+		    	FullPath=( ((IResource) obj).getLocation() ).toString();
+		    	launchCmd[4]=FullPath;
+//				    System.out.println(">" + launchCmd[0] + " " + launchCmd[1] + " " + launchCmd[2 ] + " " + launchCmd[3] + " " + launchCmd[4]);
+				MercurialUtilities.ExecuteCommand(launchCmd);
+	    	}
+	    }
+	}
+
+	
+  
+	/**
+	 * Selection in the workbench has been changed. We 
+	 * can change the state of the 'real' action here
+	 * if we want, but this can only happen after 
+	 * the delegate has been created.
+	 * @see IWorkbenchWindowActionDelegate#selectionChanged
+	 */
+	public void selectionChanged(IAction action, ISelection in_selection) 
+	{
+		if( in_selection != null && in_selection instanceof IStructuredSelection )
+		{
+			selection = ( IStructuredSelection )in_selection;
+		}
+	}
+	
+}
---
+++ src/com/vectrace/MercurialEclipse/team/MercurialConfigurationWizard.java
@@ -0,0 +1,263 @@
+/**
+ * Mercurial create Wizard
+ * 
+ * This wizard will dig up the project root and then 
+ * it lets you modify the directory, when you are done it will 
+ * create a mercurial repository
+ * 
+ * It will follow the dirictory chain to the bottom to se is
+ * there is a .hg directory someware, is so it will suggest that you use it
+ * instead of creating a new repository.
+ * 
+ */
+package com.vectrace.MercurialEclipse.team;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.team.core.RepositoryProvider;
+import org.eclipse.team.core.TeamException;
+import org.eclipse.team.ui.IConfigurationWizard;
+import org.eclipse.ui.IWorkbench;
+
+
+/**
+ * @author zingo
+ *
+ */
+public class MercurialConfigurationWizard extends Wizard implements IConfigurationWizard 
+{
+	public class NewWizardPage extends WizardPage implements SelectionListener 
+	{
+		Button changeDirButton;
+		Button restoreDefaultDirButton;
+		Button restoreExistingDirButton;
+		boolean newMercurialProject;
+		
+		NewWizardPage(boolean newMercurialProject) 
+		{
+		    super( WizardPage.class.getName() );
+		    this.newMercurialProject=newMercurialProject;
+		    if(newMercurialProject)
+		    {
+		    	setTitle( "Mercurial Setup wizard" );
+		    	setDescription( "Put this project under mercurial version control." );
+		    }
+		    else
+		    {
+		    	setTitle( "Mercurial Connect to existing repository" );
+		    	setDescription( "Connect to an existing mercurial version control" );
+		    }
+		    
+		    //		    String imgKey = "icons/sample.gif";
+//		    setImageDescriptor( new ImageDescriptor( ( imgKey ) );
+		    setPageComplete(true); /* Thel that it has the needed info */
+		  } 
+		  
+		  // interface methods of CreateRepositoryPage
+		  ////////////////////////////////////////////
+		  
+		  public void createControl( final Composite parent ) 
+		  {
+			Composite mainControl;
+			Label label;
+			
+			mainControl = new Composite(parent, SWT.NONE);
+		    mainControl.setLayout( new GridLayout(3, false) );
+
+		    label = new Label( mainControl, SWT.CENTER);
+		    label.setText("Select Directory");
+
+		    directoryText = new Text(mainControl, SWT.CENTER | SWT.SINGLE);
+		    directoryText.setText(hgPath);
+		    directoryText.addSelectionListener(this);
+		    
+		    changeDirButton = new Button(mainControl, SWT.CENTER | SWT.PUSH);
+		    changeDirButton.setText("Change Dir");
+		    changeDirButton.addSelectionListener(this);
+
+			restoreDefaultDirButton = new Button(mainControl, SWT.CENTER | SWT.PUSH);
+			restoreDefaultDirButton.setText("Use Project Root");
+			restoreDefaultDirButton.addSelectionListener(this);
+
+		    if(!newMercurialProject)
+		    {
+		    	restoreExistingDirButton = new Button(mainControl, SWT.CENTER | SWT.PUSH);
+		    	restoreExistingDirButton.setText("Use Existing .hg directory");
+		    	restoreExistingDirButton.addSelectionListener(this);
+		    }
+		    
+		    setControl(mainControl);
+		    setPageComplete(true);
+	  
+		  }
+
+		public void widgetSelected(SelectionEvent e) 
+		{
+			if(e.widget==changeDirButton )
+			{
+				DirectoryDialog directoryDialog;
+			    directoryDialog = new DirectoryDialog( new Shell() );
+			    directoryDialog.setText("Select mercurial root");
+			    directoryDialog.setMessage("Select mercurial root, should be project root.");
+			 
+			    hgPath=directoryDialog.open();
+				directoryText.setText(hgPath);
+				//directoryDialog.close();
+			}
+			else if(e.widget == restoreDefaultDirButton)
+			{
+				hgPath=hgPathOrginal;
+				directoryText.setText(hgPath);
+			}
+			else if(e.widget == directoryText)
+			{
+				hgPath=directoryText.getText();
+				directoryText.setText(hgPath);
+			}
+			else if((!newMercurialProject) && (e.widget == restoreExistingDirButton))
+			{
+				hgPath=foundhgPath;
+				directoryText.setText(hgPath);
+			}
+		}
+
+		public void widgetDefaultSelected(SelectionEvent e) 
+		{
+			//System.out.println("widgetDefaultSelected");
+			if(e.widget == directoryText)
+			{
+				hgPath=directoryText.getText();
+				directoryText.setText(hgPath);
+			}
+//      	This should not happend from a button widget.
+		}
+	}
+	
+	private IProject project;
+	private String hgPath;
+	private String hgPathOrginal;
+	private String foundhgPath;
+//	private String pathProject;
+	private Text directoryText;
+	
+	
+	/**
+	 * 
+	 */
+	public MercurialConfigurationWizard() {
+		//super();
+	    setWindowTitle( "MercurialConfigurationWizard" );
+//		System.out.println("MercurialConfigurationWizard.MercurialConfigurationWizard()");
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.wizard.IWizard#addPages()
+	 */
+	public void addPages() {
+//		System.out.println("MercurialConfigurationWizard.addPages()");
+
+//	    IPath projectLocation = project.getLocation();
+	    String MercurialRootDir;
+	    MercurialRootDir=MercurialUtilities.search4MercurialRoot(project);
+//	    MercurialRootDir=MercurialUtilities.search4MercurialRoot(projectLocation.toFile());
+//	    pathProject=project.getLocation().toString();
+	    if (MercurialRootDir == null)
+	    {   
+	    	foundhgPath=null;
+	    	hgPathOrginal=project.getLocation().toString();
+	    	hgPath=hgPathOrginal;
+			addPage( new NewWizardPage(true) );
+	    }
+	    else
+	    {
+	    	foundhgPath=MercurialRootDir;
+	    	hgPathOrginal=MercurialRootDir;
+	    	hgPath=hgPathOrginal;
+			addPage( new NewWizardPage(false) );
+	    }
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.wizard.IWizard#performFinish()
+	 */
+	public boolean performFinish() {
+		if(directoryText!=null)
+		{
+			hgPath=directoryText.getText();
+		}
+//		System.out.println("MercurialConfigurationWizard.preformFinish()");
+//		System.out.println("Path:" + hgPath);
+		if( (foundhgPath==null) ||  (! foundhgPath.equals(hgPath) ) )
+		{
+			String launchCmd[] = { "hg", "init", hgPath };
+			try 
+			{
+				String line;
+				Process process = Runtime.getRuntime().exec(launchCmd); 
+				BufferedReader input = new BufferedReader( new InputStreamReader(process.getInputStream()));
+		        while ((line = input.readLine()) != null) 
+		        {
+//TODO output this text nice:er
+		        	System.out.println(line);
+				}
+				input.close();
+				/*int exitVal =*/ process.waitFor();
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+				return false;
+			} 
+			catch (InterruptedException e) 
+			{
+				e.printStackTrace();
+				return false;
+			}			
+		}
+		try 
+		{
+			RepositoryProvider.map( project, MercurialTeamProvider.class.getName() );
+			MercurialTeamProvider hgProvider; 
+			hgProvider = (MercurialTeamProvider) RepositoryProvider.getProvider( project );
+		    hgProvider.setRepositoryPath( hgPath );
+			
+		} 
+		catch (TeamException e) 
+		{
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.team.ui.IConfigurationWizard#init(org.eclipse.ui.IWorkbench, org.eclipse.core.resources.IProject)
+	 */
+	public void init(IWorkbench workbench, IProject project) {
+		//System.out.println("MercurialConfigurationWizard.init()");
+		this.project=project;
+	}
+
+	
+
+	
+
+}
