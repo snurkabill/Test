830106f0733cd29923cfee3f29cf882b934cb816
zingo <zingo@localhost>
2006-05-26 00:05:52 +0200

    Added missing Preference source files, Ticket #6 is integrated now and it is working, patch didn't contained this missing files, so I made the content up, thanks to Gunnar Ahlberg for getting this configurable

---
+++ src/com/vectrace/MercurialEclipse/preferences/MercurialPreferenceConstants.java
@@ -0,0 +1,11 @@
+package com.vectrace.MercurialEclipse.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class MercurialPreferenceConstants
+{
+  public static final String MERCURIAL_EXECUTABLE = "hg";
+}
+
+
---
+++ src/com/vectrace/MercurialEclipse/team/Console.java
@@ -0,0 +1,22 @@
+/**
+ * com.vectrace.MercurialEclipse (c) Vectrace Feb 11, 2006
+ * Created by zingo
+ */
+package com.vectrace.MercurialEclipse.team;
+
+/**
+ * @author zingo
+ *
+ */
+public class Console
+{
+
+	/**
+	 * 
+	 */
+	public Console() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
