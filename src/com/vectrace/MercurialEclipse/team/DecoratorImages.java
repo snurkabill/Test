171e5b5cd6d7be9cbf44ed511c0993294df31b91
zingo <zingo@localhost>
2007-03-28 21:39:58 +0200

    Fix repository below project root see ticket #61 for more info.

--- src/com/vectrace/MercurialEclipse/team/DecoratorImages.java
+++ src/com/vectrace/MercurialEclipse/team/DecoratorImages.java
@@ -16,36 +16,36 @@ public class DecoratorImages
   /**
    * Added Image Descriptor
    */ 
-  public static final ImageDescriptor addedDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/mercury/added.gif");
+  public static final ImageDescriptor addedDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/added.png");
   
   /**
    * Deleted but still tracked Image Descriptor
    */ 
-  public static final ImageDescriptor deletedStillTrackedDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/mercury/deleted_still_tracked.gif");
+  public static final ImageDescriptor deletedStillTrackedDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/deleted_still_tracked.png");
   
   /**
    * Ignored Image Descriptor
    */ 
-  public static final ImageDescriptor ignoredDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/mercury/ignored.gif");
+  public static final ImageDescriptor ignoredDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/ignored.png");
     
   /**
    * Modified Image Descriptor
    */ 
-  public static final ImageDescriptor modifiedDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/mercury/modified.gif");
+  public static final ImageDescriptor modifiedDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/modified.png");
 
   /**
    * Not tracked Image Descriptor
    */ 
-  public static final ImageDescriptor notTrackedDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/mercury/not_tracked.gif");
+  public static final ImageDescriptor notTrackedDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/not_tracked.png");
   /**
    * Removed Image Descriptor
    */ 
-  public static final ImageDescriptor removedDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/mercury/removed.gif");
+  public static final ImageDescriptor removedDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/removed.png");
 
   /**
    * Managed Image Descriptor
    */ 
-  public static final ImageDescriptor managedDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/mercury/managed.gif");
+  public static final ImageDescriptor managedDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/managed.png");
   
   
   /**
