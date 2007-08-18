f10f5494e8dc3e209038531732389f7b39682e48
zingo <zingo@localhost>
2007-08-18 22:27:04 +0200

    #86 Added changes from Sebastian

--- src/com/vectrace/MercurialEclipse/model/ChangeLog.java
+++ src/com/vectrace/MercurialEclipse/model/ChangeLog.java
@@ -29,8 +29,8 @@ public class ChangeLog
     String token;
 
     
-    StringTokenizer st = new StringTokenizer(changeLog);
     String eol = System.getProperty("line.separator");
+    StringTokenizer st = new StringTokenizer(changeLog, eol);
     
 /*
 changeset:   39:9224c59c4f17
@@ -66,115 +66,109 @@ Moved Prefs to team section fixes ticket #16
 
     while(st.hasMoreTokens())
     {
-      
-      changeset=null;
-      tag=null;
-      user=null;
-      date=null;
-      files=null;
-      description=null;
 
-      
 //      System.out.println("token: <" + token + ">");
       if(token.startsWith("changeset:")) 
       {
-        //Start of a "changeset:"
+
         int index;
-        index="changeset:".length();
-        while(token.charAt(index)==' ') //remove begining spaces
-        {
+        boolean moredata = true;
+        
+        changeset=null;
+        tag=null;
+        user=null;
+        date=null;
+        files=null;
+        description=null;
+
+        index = "changeset:".length();
+        
+        while(token.charAt(index)==' ') 
+        { //remove begining spaces
           index++;
         }
-        changeset=token.substring(index);
         
-        if(st.hasMoreTokens())
-        {  
-          token = st.nextToken(eol);
-        }
-//        System.out.println("cs_token: <" + token + ">");
-        boolean moredata = true;
-        while(!token.startsWith("changeset:") && moredata)
+        changeset = token.substring(index);
+      
+        while (st.hasMoreTokens() && moredata) 
         {
-          if(token.startsWith("tag:"))
+          
+          token = st.nextToken(eol);
+          
+          if (token.startsWith("tag:")) 
           {
-            index="tag:".length();
-            while(token.charAt(index)==' ') //remove begining spaces
-            {
-              index++;
-            }
-            tag=token.substring(index);            
-          }
-          else if(token.startsWith("user:"))
-          {
-            index="user:".length();
-            while(token.charAt(index)==' ') //remove begining spaces
-            {
-              index++;
-            }
-            user=token.substring(index);            
+            index = "tag:".length();
             
-          }
-          else if(token.startsWith("date:"))
+              while(token.charAt(index)==' ') 
+              { //remove begining spaces
+                index++;
+              }
+              tag = token.substring(index);
+              
+          } 
+          else if (token.startsWith("user:")) 
           {
-            index="date:".length();
-            while(token.charAt(index)==' ') //remove begining spaces
-            {
-              index++;
-            }
-            date=token.substring(index);            
+            index = "user:".length();
             
-          }
-          else if(token.startsWith("files:"))
+              while(token.charAt(index)==' ') 
+              { //remove begining spaces
+                index++;
+              }
+
+            user = token.substring(index);
+          } 
+          else if (token.startsWith("date:")) 
           {
-            index="files:".length();
-            while(token.charAt(index)==' ') //remove begining spaces
-            {
-              index++;
-            }
-            files=token.substring(index);            
+            index = "date:".length();
             
-          }
-          else if(token.startsWith("description:"))
+              while(token.charAt(index)==' ') 
+              { //remove begining spaces
+                index++;
+              }
+              
+              date = token.substring(index);
+          } 
+          else if (token.startsWith("files:")) 
           {
-            if(st.hasMoreTokens())
-            {  
-              description=st.nextToken(eol); //TODO: get more then one line
-            }
-            else
-            {
-              moredata = false;           
-            }
-          }
-          if(st.hasMoreTokens())
-          {  
-            token = st.nextToken(eol);
-          }
-          else
+            index = "files:".length();
+
+              while(token.charAt(index)==' ') 
+              { //remove begining spaces
+                index++;
+              }
+              
+              files = token.substring(index);
+          } 
+          else if (token.equals("description:")) 
           {
-            moredata = false;           
+            
+            description = st.nextToken(eol);
+            
+            while (st.hasMoreTokens() && moredata) {
+              token = st.nextToken(eol);
+              
+              if(token.startsWith("changeset:")) 
+              {
+                moredata = false;
+              } 
+              else 
+              {
+                description = description + " | " + token;
+              }
+
+            }
+
           }
-//          System.out.println("cs_token: <" + token + ">");
+
         }
 
-        
-//        System.out.println("changeset:   <" + changeset + ">");
-//        System.out.println("tag:         <" + tag + ">");
-//        System.out.println("user:        <" + user + ">");
-//        System.out.println("date:        <" + date + ">");
-//        System.out.println("files:       <" + files + ">");
-//        System.out.println("description: <" + description + ">");
-        this.changeLog.add( new ChangeSet(changeset,tag,user,date,files,description));
-      }
-      else
-      {
-        if(st.hasMoreTokens())
-        {  
-          token = st.nextToken(eol);
-        }
-        else
-        {
-          continue;          
-        }
+//          System.out.println("changeset:   <" + changeset + ">");
+//          System.out.println("tag:         <" + tag + ">");
+//          System.out.println("user:        <" + user + ">");
+//          System.out.println("date:        <" + date + ">");
+//          System.out.println("files:       <" + files + ">");
+//          System.out.println("description: <" + description + ">");
+          this.changeLog.add( new ChangeSet(changeset,tag,user,date,files,description));
       }
     }        
 //    System.out.println("Done!!!");
