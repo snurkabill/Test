98186a402a84855e285d241ec5d6be8030b7e85e
Jerome Negre <jerome+hg@jnegre.org>
2008-04-05 22:08:36 +0200

    One can now merge with whatever revision

--- src/com/vectrace/MercurialEclipse/team/MergeAction.java
+++ src/com/vectrace/MercurialEclipse/team/MergeAction.java
@@ -3,10 +3,12 @@ package com.vectrace.MercurialEclipse.team;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.QualifiedName;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.ui.PlatformUI;
 
 import com.vectrace.MercurialEclipse.MercurialEclipsePlugin;
 import com.vectrace.MercurialEclipse.commands.HgIMergeClient;
+import com.vectrace.MercurialEclipse.dialogs.RevisionChooserDialog;
 import com.vectrace.MercurialEclipse.views.MergeView;
 
 public class MergeAction extends SingleResourceAction {
@@ -18,12 +20,17 @@ public class MergeAction extends SingleResourceAction {
     protected void run(IResource resource) throws Exception {
         IProject project = resource.getProject();
         if(project.getPersistentProperty(new QualifiedName(MercurialEclipsePlugin.ID, MERGING_PROPERTY)) == null) {
-            HgIMergeClient.merge(project, null);//FIXME handle other revisions
-            project.setPersistentProperty(new QualifiedName(MercurialEclipsePlugin.ID, MERGING_PROPERTY), "true");
-            project.refreshLocal(IResource.DEPTH_INFINITE, null);
-            //TODO update Merge view
-            PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(
-                    MergeView.ID);
+            RevisionChooserDialog dialog = new RevisionChooserDialog(getShell(),
+                    "Merge With...", project);
+            if (dialog.open() == IDialogConstants.OK_ID) {
+                HgIMergeClient.merge(project, dialog.getRevision());
+                project.setPersistentProperty(new QualifiedName(MercurialEclipsePlugin.ID, MERGING_PROPERTY), "true");
+                project.refreshLocal(IResource.DEPTH_INFINITE, null);
+                //TODO update Merge view
+                PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(
+                        MergeView.ID);
+            }
+
         }
     }
 
