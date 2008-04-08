6740bb3cb5a8e33ec2dda01e630d452799379440
Charles O'Farrell <charleso@gmail.com>
2008-04-09 08:54:06 +1000

    Fixed my own unit tests

--- test/com/vectrace/MercurialEclipse/annotations/AnnotateCommandTest.java
+++ test/com/vectrace/MercurialEclipse/annotations/AnnotateCommandTest.java
@@ -1,11 +1,19 @@
 package com.vectrace.MercurialEclipse.annotations;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.TimeZone;
 
 import junit.framework.TestCase;
 
 public class AnnotateCommandTest extends TestCase
 {
+  public static final DateFormat DATE_FORMAT = new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy Z");
+  static {
+    DATE_FORMAT.setTimeZone(TimeZone.getTimeZone("UTC"));
+  }
+  
   private List<AnnotateBlock> createFromStdOut(String name)
   {
     return AnnotateCommand.createFromStdOut(
@@ -17,17 +25,17 @@ public class AnnotateCommandTest extends TestCase
     List<AnnotateBlock> blocks = createFromStdOut("annotate1");
     assertEquals(48, blocks.size());
 
-    blah(blocks, 0, "zingo", "146:16cd70529433", "Tue Feb 05 21:17:52 2008 +0100", 0, 0);
-    blah(blocks, 1, "zingo", "151:893d61d581c6", "Thu Mar 27 22:47:06 2008 +0100", 1, 5);
+    checkBlock(blocks, 0, "zingo", "146:16cd70529433", "Tue Feb 05 20:17:52 2008 +0000", 0, 0);
+    checkBlock(blocks, 1, "zingo", "151:893d61d581c6", "Thu Mar 27 21:47:06 2008 +0000", 1, 5);
   }
 
-  private void blah(List<AnnotateBlock> blocks, int i, String user, String rev, String date, int startLine, int endLine)
+  private void checkBlock(List<AnnotateBlock> blocks, int i, String user, String rev, String date, int startLine, int endLine)
   {
     AnnotateBlock block = blocks.get(i);
     assertEquals(user, block.getUser());
     assertEquals(rev, block.getRevision().toString());
     assertEquals(startLine, block.getStartLine());
     assertEquals(endLine, block.getEndLine());
-    assertEquals(date, AnnotateCommand.DATE_FORMAT.format(block.getDate()));
+    assertEquals(date, DATE_FORMAT.format(block.getDate()));
   }
 }
