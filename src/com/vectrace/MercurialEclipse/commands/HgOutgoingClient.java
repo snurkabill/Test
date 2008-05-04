6672ecafadfe6314712aba9abd8ed30fe5144ea2
Bastian Doetsch <bastian.doetsch@gmx.de>
2008-05-04 23:09:21 +0200

    fixed exception If no outgoing changes were found.
    now interpreting return code = 1 correctly.

--- src/com/vectrace/MercurialEclipse/commands/HgOutgoingClient.java
+++ src/com/vectrace/MercurialEclipse/commands/HgOutgoingClient.java
@@ -14,19 +14,27 @@ public class HgOutgoingClient {
 
     public static Map<IResource, SortedSet<ChangeSet>> getOutgoing(
             IResource res, HgRepositoryLocation loc) throws HgException {
-        HgCommand command = new HgCommand("outgoing", res.getProject(), false);
+        try {
+            HgCommand command = new HgCommand("outgoing", res.getProject(),
+                    false);
 
-        command.addOptions("--template", HgIncomingClient.TEMPLATE);
+            command.addOptions("--template", HgIncomingClient.TEMPLATE);
 
-        command.addOptions(loc.toString());
-        String result = command.executeToString();
-        if (result.contains("no changes found")) {
-            return null;
+            command.addOptions(loc.toString());
+            String result = command.executeToString();
+            if (result.contains("no changes found")) {
+                return null;
+            }
+            Map<IResource, SortedSet<ChangeSet>> revisions = HgIncomingClient
+                    .createMercurialRevisions(result, res.getProject(), null,
+                            loc, Direction.OUTGOING);
+            return revisions;
+        } catch (HgException hg) {
+            if (hg.getMessage().contains("return code: 1")) {
+                return null;
+            }
+            throw new HgException(hg.getMessage(), hg);
         }
-        Map<IResource, SortedSet<ChangeSet>> revisions = HgIncomingClient
-                .createMercurialRevisions(result, res.getProject(), null, loc,
-                        Direction.OUTGOING);
-        return revisions;
     }
 
 }
