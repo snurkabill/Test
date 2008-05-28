0f998d66c9c8262680595e901f7d2d8afc4c59a8
HemPc <HemPc@PC212826566277>
2008-05-28 22:03:48 +0200

    Added isBisecting check and parsing of bisect.state file. UI Code still needs to be worked out (not happy with my implementation, therefore not commited)

--- src/com/vectrace/MercurialEclipse/commands/HgBisectClient.java
+++ src/com/vectrace/MercurialEclipse/commands/HgBisectClient.java
@@ -10,8 +10,14 @@
  *******************************************************************************/
 package com.vectrace.MercurialEclipse.commands;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import com.vectrace.MercurialEclipse.MercurialEclipsePlugin;
 import com.vectrace.MercurialEclipse.exception.HgException;
 import com.vectrace.MercurialEclipse.model.ChangeSet;
 
@@ -21,6 +27,8 @@ import com.vectrace.MercurialEclipse.model.ChangeSet;
  */
 public final class HgBisectClient {
 
+    public enum Status { GOOD, BAD }
+    
     public static String markGood(File repository, ChangeSet good)
             throws HgException {
         HgCommand cmd = new HgCommand("bisect", repository, true);
@@ -39,6 +47,45 @@ public final class HgBisectClient {
         HgCommand cmd = new HgCommand("bisect", repository, true);
         cmd.addOptions("-r");
         return cmd.executeToString();
+    }
+    
+    public static boolean isBisecting(File repository) {
+        try {
+            File file = getStatusFile(repository);
+            return file.exists();
+        } catch (IOException e) {
+            return false;
+        }
+    }
+    
+    public static Map<String, Status> getBisectStatus(File repository) {
+        HashMap<String, Status> statusByRevision = new HashMap<String, Status>();
+        if(!isBisecting(repository)) {
+            return statusByRevision;
+        }
+        try {
+            File file = getStatusFile(repository);
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while(null != (line = reader.readLine())) {
+                String[] statusChangeset = line.split("\\s");
+                if(statusChangeset[0].equalsIgnoreCase("bad")) {
+                    statusByRevision.put(statusChangeset[1].trim(), Status.BAD);
+                } else {
+                    statusByRevision.put(statusChangeset[1].trim(), Status.GOOD);
+                }
+            }
+        } catch (IOException e) {
+            MercurialEclipsePlugin.logError(e);
+        }
+        return statusByRevision;
+    }
+
+    private static File getStatusFile(File repository) throws IOException {
+        String root = repository.getCanonicalPath();
+        String bisectStatusFile = root + File.separator + ".hg" + File.separator + "bisect.state";
+        File file = new File(bisectStatusFile);
+        return file;
     }
 
     private static String getRevision(ChangeSet change) {
