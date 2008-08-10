6cfd346ab0737b902ce5c7986a27ae1f7b5e271d
Bastian Doetsch <bastian.doetsch@gmx.de>
2008-08-10 10:46:59 +0200

    added license information

--- src/com/vectrace/MercurialEclipse/commands/HgCommitClient.java
+++ src/com/vectrace/MercurialEclipse/commands/HgCommitClient.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2008 VecTrace (Zingo Andersen) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Jerome Negre              - implementation
+ *     Bastian Doetsch
+ *     StefanC
+ *******************************************************************************/
 package com.vectrace.MercurialEclipse.commands;
 
 import java.io.File;
