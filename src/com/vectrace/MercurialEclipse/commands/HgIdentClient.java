66c71cfefea599d31c8584d935e0fc469a1e3ffb
iloveeclipse <iloveeclipse@localhost>
2009-07-25 14:53:06 +0200

    Performance: do not read 20x from file stream, just read 20 bytes in one shot :-)

--- src/com/vectrace/MercurialEclipse/commands/HgIdentClient.java
+++ src/com/vectrace/MercurialEclipse/commands/HgIdentClient.java
@@ -18,31 +18,52 @@ public class HgIdentClient extends AbstractClient {
         AbstractShellCommand command = new HgCommand("identify", root, true); //$NON-NLS-1$
         command.addOptions("-n", "-i"); //$NON-NLS-1$ //$NON-NLS-2$
         command
-                .setUsePreferenceTimeout(MercurialPreferenceConstants.STATUS_TIMEOUT);
+        .setUsePreferenceTimeout(MercurialPreferenceConstants.STATUS_TIMEOUT);
         return command.executeToString().trim();
     }
 
     public static String getCurrentRevision(IResource resource)
-            throws HgException {
+    throws HgException {
         AbstractShellCommand command = new HgCommand("identify", //$NON-NLS-1$
                 getWorkingDirectory(resource), true);
         command
-                .setUsePreferenceTimeout(MercurialPreferenceConstants.STATUS_TIMEOUT);
+        .setUsePreferenceTimeout(MercurialPreferenceConstants.STATUS_TIMEOUT);
         command.addOptions("-nibt"); //$NON-NLS-1$
         return command.executeToString().trim();
     }
-    
+
+    /**
+     * @param b
+     *            byte array containing integer data
+     * @param idx
+     *            start index to read integer from
+     * @return integer value, corresponding to the 4 bytes from the given array at given position
+     */
+    private static int getNextInt(byte[] b, int idx) {
+        int result = 0;
+        for(int i = 0; i < 4 && i + idx < b.length; i++) {
+            result = ((result << 8) + (b[i + idx] & 0xff));
+        }
+        return result;
+    }
+
     static String getCurrentChangesetId(InputStream inputStream) throws IOException {
-        StringBuilder id = new StringBuilder();
-        for (int i = 0, x = 0; i < 20 && (x = inputStream.read()) != -1; ++i) {
-            String s = Integer.toHexString(x);
-            if (s.length() == 1) {
-                s = "0" + s; //$NON-NLS-1$
+        StringBuilder id = new StringBuilder(20);
+        byte[] first20bytes = new byte[20];
+        int read = inputStream.read(first20bytes);
+        for (int i = 0; i < read; i += 4) {
+            int next = getNextInt(first20bytes, i);
+            String s = Integer.toHexString(next);
+            int size = s.length();
+            while(size < 8) {
+                id.append('0');
+                size ++;
             }
             id.append(s);
         }
         return id.toString();
     }
+
 
     /**
      * Returns the current node-id as a String
@@ -54,12 +75,13 @@ public class HgIdentClient extends AbstractClient {
      * @throws IOException
      */
     public static String getCurrentChangesetId(File repository)
-            throws HgException, IOException {
-        String dirstate = repository.getCanonicalPath() + File.separator
-                + ".hg" + File.separator + "dirstate"; //$NON-NLS-1$ //$NON-NLS-2$
+    throws HgException, IOException {
+        StringBuilder pathStr = new StringBuilder(repository.getCanonicalPath());
+        pathStr.append(File.separator).append(".hg");
+        pathStr.append(File.separator).append("dirstate");
         FileInputStream reader;
         try {
-            reader = new FileInputStream(dirstate);
+            reader = new FileInputStream(pathStr.toString());
         } catch (FileNotFoundException e) {
             return null;
         }
