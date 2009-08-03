ef1f9198839c7b600249d92413533f33c23829db
iloveeclipse <iloveeclipse@localhost>
2009-08-03 18:22:24 +0200

    Removed obsoleted DecoratorStatus class (replaced by ResourceDecorator), fixed unneeded references to it in the DecoratorImages

--- src/com/vectrace/MercurialEclipse/team/DecoratorImages.java
+++ src/com/vectrace/MercurialEclipse/team/DecoratorImages.java
@@ -16,49 +16,49 @@ import org.eclipse.jface.resource.ImageDescriptor;
 /**
  * Set of images that are used for decorating resources are maintained
  * here. This acts as a image registry and hence there is a single copy
- * of the image files floating around the project. 
- * 
+ * of the image files floating around the project.
+ *
  */
 public class DecoratorImages
 {
   /**
    * Added Image Descriptor
-   */ 
-  public static final ImageDescriptor addedDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/added_ov.gif"); //$NON-NLS-1$
-  
+   */
+  public static final ImageDescriptor addedDescriptor = ImageDescriptor.createFromFile (DecoratorImages.class, "images/added_ov.gif"); //$NON-NLS-1$
+
   /**
    * Deleted but still tracked Image Descriptor
-   */ 
-  public static final ImageDescriptor deletedStillTrackedDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/deleted_still_tracked_ov.gif"); //$NON-NLS-1$
-  
+   */
+  public static final ImageDescriptor deletedStillTrackedDescriptor = ImageDescriptor.createFromFile (DecoratorImages.class, "images/deleted_still_tracked_ov.gif"); //$NON-NLS-1$
+
   /**
    * Ignored Image Descriptor
-   */ 
-  public static final ImageDescriptor ignoredDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/ignored_ov.gif"); //$NON-NLS-1$
-    
+   */
+  public static final ImageDescriptor ignoredDescriptor = ImageDescriptor.createFromFile (DecoratorImages.class, "images/ignored_ov.gif"); //$NON-NLS-1$
+
   /**
    * Modified Image Descriptor
-   */ 
-  public static final ImageDescriptor modifiedDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/modified_ov.gif"); //$NON-NLS-1$
+   */
+  public static final ImageDescriptor modifiedDescriptor = ImageDescriptor.createFromFile (DecoratorImages.class, "images/modified_ov.gif"); //$NON-NLS-1$
 
   /**
    * Not tracked Image Descriptor
-   */ 
-  public static final ImageDescriptor notTrackedDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/not_tracked_ov.gif"); //$NON-NLS-1$
+   */
+  public static final ImageDescriptor notTrackedDescriptor = ImageDescriptor.createFromFile (DecoratorImages.class, "images/not_tracked_ov.gif"); //$NON-NLS-1$
   /**
    * Removed Image Descriptor
-   */ 
-  public static final ImageDescriptor removedDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/removed_ov.gif"); //$NON-NLS-1$
+   */
+  public static final ImageDescriptor removedDescriptor = ImageDescriptor.createFromFile (DecoratorImages.class, "images/removed_ov.gif"); //$NON-NLS-1$
 
   /**
    * Managed Image Descriptor
-   */ 
-  public static final ImageDescriptor managedDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/managed_ov.gif"); //$NON-NLS-1$
-  
+   */
+  public static final ImageDescriptor managedDescriptor = ImageDescriptor.createFromFile (DecoratorImages.class, "images/managed_ov.gif"); //$NON-NLS-1$
+
   /**
    * Conflict Image Descriptor
-   */ 
+   */
   //FIXME temporary picture
-  public static final ImageDescriptor conflictDescriptor = ImageDescriptor.createFromFile (DecoratorStatus.class, "images/confchg_ov.gif"); //$NON-NLS-1$
-  
+  public static final ImageDescriptor conflictDescriptor = ImageDescriptor.createFromFile (DecoratorImages.class, "images/confchg_ov.gif"); //$NON-NLS-1$
+
 }
