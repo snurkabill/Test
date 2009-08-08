c342aca2eb6c24bb5ca03f80e270c83370dcc9a0
iloveeclipse <iloveeclipse@localhost>
2009-08-08 23:44:09 +0200

    Test should not fail on non-english systems

--- test/com/vectrace/MercurialEclipse/annotations/AnnotateCommandTest.java
+++ test/com/vectrace/MercurialEclipse/annotations/AnnotateCommandTest.java
@@ -3,39 +3,37 @@ package com.vectrace.MercurialEclipse.annotations;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 import java.util.TimeZone;
 
 import junit.framework.TestCase;
 
 public class AnnotateCommandTest extends TestCase
 {
-  public static final DateFormat DATE_FORMAT = new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy Z");
-  static {
-    DATE_FORMAT.setTimeZone(TimeZone.getTimeZone("UTC"));
-  }
-  
-  private List<AnnotateBlock> createFromStdOut(String name)
-  {
-    return AnnotateCommand.createFromStdOut(
-        getClass().getResourceAsStream(name + ".out")).getAnnotateBlocks();
-  }
+    public static final DateFormat DATE_FORMAT = new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy Z", Locale.ENGLISH);
+    static {
+        DATE_FORMAT.setTimeZone(TimeZone.getTimeZone("UTC"));
+    }
 
-  public void test1()
-  {
-    List<AnnotateBlock> blocks = createFromStdOut("annotate1");
-    assertEquals(48, blocks.size());
+    private List<AnnotateBlock> createFromStdOut(String name) {
+        return AnnotateCommand.createFromStdOut(
+                getClass().getResourceAsStream(name + ".out")).getAnnotateBlocks();
+    }
 
-    checkBlock(blocks, 0, "zingo", "146:16cd70529433", "Tue Feb 05 20:17:52 2008 +0000", 0, 0);
-    checkBlock(blocks, 1, "zingo", "151:893d61d581c6", "Thu Mar 27 21:47:06 2008 +0000", 1, 5);
-  }
+    public void test1() {
+        List<AnnotateBlock> blocks = createFromStdOut("annotate1");
+        assertEquals(48, blocks.size());
 
-  private void checkBlock(List<AnnotateBlock> blocks, int i, String user, String rev, String date, int startLine, int endLine)
-  {
-    AnnotateBlock block = blocks.get(i);
-    assertEquals(user, block.getUser());
-    assertEquals(rev, block.getRevision().toString());
-    assertEquals(startLine, block.getStartLine());
-    assertEquals(endLine, block.getEndLine());
-    assertEquals(date, DATE_FORMAT.format(block.getDate()));
-  }
+        checkBlock(blocks, 0, "zingo", "146:16cd70529433", "Tue Feb 05 20:17:52 2008 +0000", 0, 0);
+        checkBlock(blocks, 1, "zingo", "151:893d61d581c6", "Thu Mar 27 21:47:06 2008 +0000", 1, 5);
+    }
+
+    private void checkBlock(List<AnnotateBlock> blocks, int i, String user, String rev, String date, int startLine, int endLine){
+        AnnotateBlock block = blocks.get(i);
+        assertEquals(user, block.getUser());
+        assertEquals(rev, block.getRevision().toString());
+        assertEquals(startLine, block.getStartLine());
+        assertEquals(endLine, block.getEndLine());
+        assertEquals(date, DATE_FORMAT.format(block.getDate()));
+    }
 }
