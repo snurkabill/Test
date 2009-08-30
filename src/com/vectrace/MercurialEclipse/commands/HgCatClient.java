6f2986bf1273808780c207ac8618e68988b74cd7
Martin Kneissl <martin@kneissl.eu>
2009-08-30 10:03:32 +0200

    Now using an existing utility method to make the path for hg cat relative to the root of the working copy.

--- src/com/vectrace/MercurialEclipse/commands/HgCatClient.java
+++ src/com/vectrace/MercurialEclipse/commands/HgCatClient.java
@@ -1,7 +1,5 @@
 package com.vectrace.MercurialEclipse.commands;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,36 +39,10 @@ public class HgCatClient {
         command.add("--decode"); //$NON-NLS-1$
 
         HgRoot hgRoot = MercurialTeamProvider.getHgRoot(file);
-        command.add(pathRelativeTo(file, hgRoot));
+        command.add(hgRoot.toRelative(file.getLocation().toFile()));
 
         AbstractShellCommand hgCommand = new HgCommand(command, hgRoot, true);
 
         return hgCommand.executeToString();
-    }
-
-    /**
-     * Construct a path relative to the mercurial working copy root.
-     *
-     * @param file create relative path for this file.
-     * @param hgRoot the working copy root.
-     * @return a path to <code>file<code> relative to <code>hgRoot</code>
-     *
-     * @throws HgException
-     */
-    private static String pathRelativeTo(IFile file, HgRoot hgRoot) throws HgException {
-        String rootPath = hgRoot.getAbsolutePath();
-        String filePath;
-        try {
-            filePath = file.getLocation().toFile().getCanonicalPath();
-        } catch (IOException e) {
-            throw new HgException("Can't resolve path of " + file + ": " + e, e);
-        }
-        if (filePath.startsWith(rootPath)) {
-            filePath = filePath.substring(rootPath.length());
-        }
-        if (filePath.startsWith(File.separator)) {
-            filePath = filePath.substring(File.separator.length());
-        }
-        return filePath;
     }
 }
