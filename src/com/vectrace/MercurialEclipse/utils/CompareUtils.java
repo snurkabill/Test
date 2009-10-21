9eb6128d835b9c1cad244bc60852a9c138f0eba8
zsolt <zsolt.koppany@intland.com>
2009-10-21 19:13:19 +0200

    #10369 Workaround in \"CompareUtils.openEditor\" if \"changeset\" is null.

--- src/com/vectrace/MercurialEclipse/utils/CompareUtils.java
+++ src/com/vectrace/MercurialEclipse/utils/CompareUtils.java
@@ -16,115 +16,94 @@ import com.vectrace.MercurialEclipse.team.MercurialRevisionStorage;
 /**
  * This class helps to invoke the compare facilities of Eclipse.
  * @author bastian
- *
  */
 public class CompareUtils {
+	public static void openEditor(IResource file, ChangeSet changeset, boolean dialog, boolean localEditable) {
+		int changesetIndex = changeset == null ? 0 : changeset.getChangesetIndex();
+		String changesetId = changeset == null ? null : changeset.getChangeset();
 
-    public static void openEditor(IResource file, ChangeSet changeset,
-            boolean dialog, boolean localEditable) {
-        openEditor(file, new MercurialRevisionStorage(file, changeset
-                .getChangesetIndex(),
-                changeset.getChangeset(), changeset), dialog, localEditable); 
-    }
+		openEditor(file, new MercurialRevisionStorage(file, changesetIndex, changesetId, changeset), dialog, localEditable);
+	}
 
-    public static void openEditor(IResource file, ChangeSet changeset, boolean localEditable) {
-        openEditor(file, new MercurialRevisionStorage(file, changeset
-                .getChangesetIndex(),
-                changeset.getChangeset(), changeset), false, localEditable); 
-    }
+	public static void openEditor(IResource file, ChangeSet changeset, boolean localEditable) {
+		int changesetIndex = changeset == null ? 0 : changeset.getChangesetIndex();
+		String changesetId = changeset == null ? null : changeset.getChangeset();
 
-    public static void openEditor(IResource file,
-            MercurialRevisionStorage right, boolean dialog, boolean localEditable) {
-        ResourceNode leftNode = null;
-        if (file != null) {
-            leftNode = new ResourceNode(file);
-        }
-        openEditor(leftNode, getNode(right), dialog, localEditable);
-    }
+		openEditor(file, new MercurialRevisionStorage(file, changesetIndex, changesetId, changeset), false, localEditable);
+	}
 
-    public static void openEditor(MercurialRevisionStorage left,
-            MercurialRevisionStorage right, boolean dialog, boolean localEditable) {
-        ResourceNode leftNode, rightNode;
-        if (right == null) {
-            // comparing with filesystem
-            rightNode = getNode(left);
-            leftNode = new ResourceNode(rightNode.getResource());
-        } else {
-            leftNode = getNode(left);
-            rightNode = getNode(right);
-        }
-        openEditor(leftNode, rightNode, dialog, localEditable);
-    }
+	public static void openEditor(IResource file, MercurialRevisionStorage right, boolean dialog, boolean localEditable) {
+		ResourceNode leftNode = null;
+		if (file != null) {
+			leftNode = new ResourceNode(file);
+		}
+		openEditor(leftNode, getNode(right), dialog, localEditable);
+	}
 
-    public static void openEditor(ResourceNode left, ResourceNode right,
-            boolean dialog, boolean localEditable) {
-        Assert.isNotNull(right);
+	public static void openEditor(MercurialRevisionStorage left, MercurialRevisionStorage right, boolean dialog, boolean localEditable) {
+		ResourceNode leftNode, rightNode;
+		if (right == null) {
+			// comparing with file-system
+			rightNode = getNode(left);
+			leftNode = new ResourceNode(rightNode.getResource());
+		} else {
+			leftNode = getNode(left);
+			rightNode = getNode(right);
+		}
+		openEditor(leftNode, rightNode, dialog, localEditable);
+	}
 
-        if (dialog) {
-            openCompareDialog(left, right, localEditable);
-        } else {
-            CompareEditorInput compareInput = getCompareInput(left, right,
-                    localEditable);
-            if (compareInput != null) {
-                CompareUI.openCompareEditor(compareInput);
-            }
-        }
-    }
+	public static void openEditor(ResourceNode left, ResourceNode right, boolean dialog, boolean localEditable) {
+		Assert.isNotNull(right);
 
-    /**
-     * Opens a compare dialog and returns it. Unfortunately the dialog is
-     * internal Eclipse API, so it might change in future Eclipse versions.
-     *
-     * @param left
-     *            the left ResourceNode to determine the compare editor input
-     * @param right
-     *            the right ResourceNode to determine the compare editor input
-     * @param shell
-     *            the shell to use for opening the dialog
-     */
-    public static int openCompareDialog(ResourceNode left, ResourceNode right,
-            boolean localEditable) {
-        CompareEditorInput compareInput = getCompareInput(left, right,
-                localEditable);
-        return openCompareDialog(compareInput);
-    }
+		if (dialog) {
+			openCompareDialog(left, right, localEditable);
+		} else {
+			CompareEditorInput compareInput = getCompareInput(left, right, localEditable);
+			if (compareInput != null) {
+				CompareUI.openCompareEditor(compareInput);
+			}
+		}
+	}
 
-    /**
-     * Opens a compare dialog using the given input.
-     *
-     * @param myShell
-     * @param compareInput
-     * @return
-     */
-    public static int openCompareDialog(CompareEditorInput compareInput) {
-        CompareUI.openCompareDialog(compareInput);
-        return Window.CANCEL;
-    }
+	/**
+	 * Opens a compare dialog and returns it. Unfortunately the dialog is
+	 * internal Eclipse API, so it might change in future Eclipse versions.
+	 *
+	 * @param left
+	 *            the left ResourceNode to determine the compare editor input
+	 * @param right
+	 *            the right ResourceNode to determine the compare editor input
+	 * @param shell
+	 *            the shell to use for opening the dialog
+	 */
+	public static int openCompareDialog(ResourceNode left, ResourceNode right, boolean localEditable) {
+		CompareEditorInput compareInput = getCompareInput(left, right, localEditable);
+		return openCompareDialog(compareInput);
+	}
 
-    public static CompareEditorInput getCompareInput(ResourceNode left,
-            ResourceNode right, boolean localEditable) {
-        IResource resource = right.getResource();
-        ResourceNode leftNode;
+	/**
+	 * Opens a compare dialog using the given input.
+	 *
+	 * @param myShell
+	 * @param compareInput
+	 * @return
+	 */
+	public static int openCompareDialog(CompareEditorInput compareInput) {
+		CompareUI.openCompareDialog(compareInput);
+		return Window.CANCEL;
+	}
 
+	public static CompareEditorInput getCompareInput(ResourceNode left, ResourceNode right, boolean localEditable) {
+		IResource resource = right.getResource();
         // switch left to right if left is null and put local to left
-        if (left != null) {
-            leftNode = left;
-        } else {
-            leftNode = right;
-        }
-        ResourceNode rightNode;
-        if (left != null) {
-            rightNode = right;
-        } else {
-            rightNode = new ResourceNode(resource);
-        }
+		ResourceNode leftNode = left != null ? left : right;
+        ResourceNode rightNode = left != null ? right : new ResourceNode(resource);
 
-        return new HgCompareEditorInput(new CompareConfiguration(), resource,
-                leftNode, rightNode);
-    }
+		return new HgCompareEditorInput(new CompareConfiguration(), resource, leftNode, rightNode);
+	}
 
-    private static RevisionNode getNode(MercurialRevisionStorage rev) {
-        return rev == null ? null : new RevisionNode(rev);
-    }
-
+	private static RevisionNode getNode(MercurialRevisionStorage rev) {
+		return rev == null ? null : new RevisionNode(rev);
+	}
 }
