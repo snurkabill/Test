6c4e8725dede6933ce2b97e478245f9a5dcfcdcc
zsolt <zsolt.koppany@intland.com>
2009-10-23 14:36:12 +0200

    Tags must be sorted and "tip" must be always the first one.

--- src/com/vectrace/MercurialEclipse/commands/HgTagClient.java
+++ src/com/vectrace/MercurialEclipse/commands/HgTagClient.java
@@ -1,5 +1,19 @@
+/*
+ * Copyright by Intland Software
+ *
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of Intland Software. ("Confidential Information"). You
+ * shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with Intland.
+ */
+
 package com.vectrace.MercurialEclipse.commands;
 
+import java.util.Collection;
+import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,54 +23,60 @@ import org.eclipse.core.resources.IResource;
 import com.vectrace.MercurialEclipse.exception.HgException;
 import com.vectrace.MercurialEclipse.model.Tag;
 
+/**
+ * @author <a href="mailto:zsolt.koppany@intland.com">Zsolt Koppany</a>
+ * @version $Id$
+ */
 public class HgTagClient {
+	private static final Pattern GET_TAGS_PATTERN = Pattern.compile("^(.+[^ ]) +([0-9]+):([a-f0-9]+)( local)?$"); //$NON-NLS-1$
 
-    private static final Pattern GET_TAGS_PATTERN = Pattern
-            .compile("^(.+[^ ]) +([0-9]+):([a-f0-9]+)( local)?$"); //$NON-NLS-1$
+	public static Tag[] getTags(IProject project) throws HgException {
+		AbstractShellCommand command = new HgCommand("tags", project, false); //$NON-NLS-1$
+		command.addOptions("-v"); //$NON-NLS-1$
+		String[] lines = command.executeToString().split("\n"); //$NON-NLS-1$
 
-    public static Tag[] getTags(IProject project) throws HgException {
-        AbstractShellCommand command = new HgCommand("tags", project, false); //$NON-NLS-1$
-        command.addOptions("-v"); //$NON-NLS-1$
-        String[] lines = command.executeToString().split("\n"); //$NON-NLS-1$
-        int length = lines.length;
-        Tag[] tags = new Tag[length];
-        for (int i = 0; i < length; i++) {
-            Matcher m = GET_TAGS_PATTERN.matcher(lines[i]);
-            if (m.matches()) {
-                Tag tag = new Tag(m.group(1), Integer.parseInt(m.group(2)), m
-                        .group(3), m.group(4) != null);
-                tags[i] = tag;
-            } else {
-                throw new HgException(Messages.getString("HgTagClient.parseException") + lines[i] + "'"); //$NON-NLS-1$ //$NON-NLS-2$
-            }
-        }
-        return tags;
-    }
+		Collection<Tag> tags = getTags(lines);
+		Tag[] sortedTags = tags.toArray(new Tag[] {});
+		return sortedTags;
+	}
 
-    /**
-     * 
-     * @param resource
-     * @param name
-     * @param user
-     *            if null, uses the default user
-     * @param local
-     * @throws HgException
-     */
-    public static void addTag(IResource resource, String name, String rev,
-            String user, boolean local, boolean force) throws HgException {
-        HgCommand command = new HgCommand("tag", resource.getProject(), false); //$NON-NLS-1$
-        if (local) {
-            command.addOptions("-l"); //$NON-NLS-1$
-        }
-        if (force) {
-            command.addOptions("-f"); //$NON-NLS-1$
-        }
-        if (rev != null) {
-            command.addOptions("-r", rev); //$NON-NLS-1$
-        }
-        command.addUserName(user);
-        command.addOptions(name);
-        command.executeToBytes();
-    }
+	protected static Collection<Tag> getTags(String[] lines) throws HgException {
+		TreeSet<Tag> tags = new TreeSet<Tag>();
 
+		for (String line : lines) {
+			Matcher m = GET_TAGS_PATTERN.matcher(line);
+			if (m.matches()) {
+				Tag tag = new Tag(m.group(1), Integer.parseInt(m.group(2)), m.group(3), m.group(4) != null);
+				tags.add(tag);
+			} else {
+				throw new HgException(Messages.getString("HgTagClient.parseException") + line + "'"); //$NON-NLS-1$ //$NON-NLS-2$
+			}
+		}
+
+		return tags;
+	}
+
+	/**
+	 * @param resource
+	 * @param name
+	 * @param user
+	 *            if null, uses the default user
+	 * @param local
+	 * @throws HgException
+	 */
+	public static void addTag(IResource resource, String name, String rev, String user, boolean local, boolean force) throws HgException {
+		HgCommand command = new HgCommand("tag", resource.getProject(), false); //$NON-NLS-1$
+		if (local) {
+			command.addOptions("-l");
+		}
+		if (force) {
+			command.addOptions("-f");
+		}
+		if (rev != null) {
+			command.addOptions("-r", rev);
+		}
+		command.addUserName(user);
+		command.addOptions(name);
+		command.executeToBytes();
+	}
 }
