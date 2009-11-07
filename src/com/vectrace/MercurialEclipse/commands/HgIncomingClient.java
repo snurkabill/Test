be2f9237e24aa5a881fe1d5114b5f635af10efa0
zsolt <zsolt.koppany@intland.com>
2009-11-07 12:10:47 +0100

    It must be ensured that the bundle will be deleted.

--- src/com/vectrace/MercurialEclipse/commands/HgIncomingClient.java
+++ src/com/vectrace/MercurialEclipse/commands/HgIncomingClient.java
@@ -5,6 +5,7 @@
  * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors: Bastian Doetsch - implementation
+ *     Zsolt Koppany zsolt.koppany@intland.com
  ******************************************************************************/
 
 package com.vectrace.MercurialEclipse.commands;
@@ -29,64 +30,69 @@ import com.vectrace.MercurialEclipse.storage.HgRepositoryLocation;
 
 public class HgIncomingClient extends AbstractParseChangesetClient {
 
-    /**
-     * Gets all File Revisions that are incoming and saves them in a bundle
-     * file. There can be more than one revision per file as this method obtains
-     * all new changesets.
-     *
-     * @return Never return null. Map containing all revisions of the IResources contained in the
-     *         Changesets. The sorting is ascending by date.
-     * @throws HgException
-     */
-    public static Map<IPath, SortedSet<ChangeSet>> getHgIncoming(IResource res,
-            HgRepositoryLocation repository) throws HgException {
-        return getHgIncoming(res, repository, null);
-    }
+	/**
+	 * Gets all File Revisions that are incoming and saves them in a bundle
+	 * file. There can be more than one revision per file as this method obtains
+	 * all new changesets.
+	 *
+	 * @return Never return null. Map containing all revisions of the IResources contained in the
+	 *         Changesets. The sorting is ascending by date.
+	 * @throws HgException
+	 */
+	public static Map<IPath, SortedSet<ChangeSet>> getHgIncoming(IResource res, HgRepositoryLocation repository) throws HgException {
+		return getHgIncoming(res, repository, null);
+	}
 
-    public static Map<IPath, SortedSet<ChangeSet>> getHgIncoming(IResource res,
-            HgRepositoryLocation repository, String branch) throws HgException {
-        AbstractShellCommand command = new HgCommand("incoming", getWorkingDirectory(res), //$NON-NLS-1$
-                false);
-        command.setUsePreferenceTimeout(MercurialPreferenceConstants.PULL_TIMEOUT);
-        if (branch != null) {
-            if (Branch.isDefault(branch)) {
-                branch = Branch.DEFAULT;
-            }
-            command.addOptions("-r", branch);
-        }
-        final File bundleFile;
-        try {
-            bundleFile = File.createTempFile("bundleFile-" + //$NON-NLS-1$
-                    res.getProject().getName() + "-", ".tmp", null); //$NON-NLS-1$ //$NON-NLS-2$
-            bundleFile.deleteOnExit();
-            command.addOptions("--debug", "--style", //$NON-NLS-1$ //$NON-NLS-2$
-                    AbstractParseChangesetClient.getStyleFile(true)
-                    .getCanonicalPath(), "--bundle", bundleFile //$NON-NLS-1$
-                    .getCanonicalPath());
-        } catch (IOException e) {
-            throw new HgException(e.getMessage(), e);
-        }
+	public static Map<IPath, SortedSet<ChangeSet>> getHgIncoming(IResource res, HgRepositoryLocation repository, String branch) throws HgException {
+		AbstractShellCommand command = new HgCommand("incoming", getWorkingDirectory(res), //$NON-NLS-1$
+				false);
+		command.setUsePreferenceTimeout(MercurialPreferenceConstants.PULL_TIMEOUT);
+		if (branch != null) {
+			if (Branch.isDefault(branch)) {
+				branch = Branch.DEFAULT;
+			}
+			command.addOptions("-r", branch);
+		}
 
-        URI uri = repository.getUri();
-        if (uri != null) {
-            command.addOptions(uri.toASCIIString());
-        } else {
-            command.addOptions(repository.getLocation());
-        }
-        try {
-            String result = command.executeToString();
-            if (result.trim().endsWith("no changes found")) { //$NON-NLS-1$
-                return new HashMap<IPath, SortedSet<ChangeSet>>();
-            }
-            Map<IPath, SortedSet<ChangeSet>> revisions = createMercurialRevisions(
-                    res, result, true,
-                    Direction.INCOMING, repository, bundleFile, new IFilePatch[0]);
-            return revisions;
-        } catch (HgException hg) {
-            if (hg.getStatus().getCode() == 1) {
-                return new HashMap<IPath, SortedSet<ChangeSet>>();
-            }
-            throw new HgException("Incoming comand failed for " + res + ". " + hg.getMessage(), hg);
-        }
-    }
+		File bundleFile = null;
+
+		try {
+			try {
+				bundleFile = File.createTempFile("bundleFile-" + //$NON-NLS-1$
+						res.getProject().getName() + "-", ".tmp", null); //$NON-NLS-1$ //$NON-NLS-2$
+				bundleFile.deleteOnExit();
+				command.addOptions("--debug", "--style", //$NON-NLS-1$ //$NON-NLS-2$
+						AbstractParseChangesetClient.getStyleFile(true)
+						.getCanonicalPath(), "--bundle", bundleFile //$NON-NLS-1$
+						.getCanonicalPath());
+			} catch (IOException e) {
+				throw new HgException(e.getMessage(), e);
+			}
+
+			URI uri = repository.getUri();
+			if (uri != null) {
+				command.addOptions(uri.toASCIIString());
+			} else {
+				command.addOptions(repository.getLocation());
+			}
+
+			try {
+				String result = command.executeToString();
+				if (result.trim().endsWith("no changes found")) { //$NON-NLS-1$
+					return new HashMap<IPath, SortedSet<ChangeSet>>();
+				}
+				Map<IPath, SortedSet<ChangeSet>> revisions = createMercurialRevisions(res, result, true, Direction.INCOMING, repository, bundleFile, new IFilePatch[0]);
+				return revisions;
+			} catch (HgException hg) {
+				if (hg.getStatus().getCode() == 1) {
+					return new HashMap<IPath, SortedSet<ChangeSet>>();
+				}
+				throw new HgException("Incoming comand failed for " + res + ". " + hg.getMessage(), hg);
+			}
+		} finally {
+			if (bundleFile != null) {
+				bundleFile.delete();
+			}
+		}
+	}
 }
