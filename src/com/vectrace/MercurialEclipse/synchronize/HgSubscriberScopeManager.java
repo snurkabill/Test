be4f4da16519d9f5b1fbc17b14b4b27cd9754cbf
hgeclipse <hgeclipse@localhost>
2009-11-15 19:50:09 +0100

    Issue #10373 : show "HG Change Sets" selector in the "sync models" drop-down menu

--- src/com/vectrace/MercurialEclipse/synchronize/HgSubscriberScopeManager.java
+++ src/com/vectrace/MercurialEclipse/synchronize/HgSubscriberScopeManager.java
@@ -23,6 +23,7 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.team.core.TeamException;
+import org.eclipse.team.core.mapping.ISynchronizationScope;
 import org.eclipse.team.core.subscribers.ISubscriberChangeEvent;
 import org.eclipse.team.core.subscribers.SubscriberScopeManager;
 import org.eclipse.ui.IPropertyListener;
@@ -58,6 +59,14 @@ public class HgSubscriberScopeManager extends SubscriberScopeManager implements 
 			}
 		};
 		MercurialTeamProvider.addBranchListener(branchListener);
+	}
+
+	@Override
+	public ISynchronizationScope getScope() {
+		// Does NOT return expected (our) scope, but ResourceMappingScope...
+		// super.getScope();
+		RepositorySynchronizationScope scope2 = ((MercurialSynchronizeSubscriber) getSubscriber()).getScope();
+		return scope2;
 	}
 
 	public void update(Observable o, Object arg) {
