4a0e8c4c9d88ad9beae52c79444b7736f6046292
hgeclipse <hgeclipse@localhost>
2009-11-21 23:20:27 +0100

    Enabled action provider for resources (open with... etc). To be continued.

---
+++ src/com/vectrace/MercurialEclipse/synchronize/cs/HgChangeSetActionProvider.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2009 VecTrace (Zingo Andersen) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Andrei Loskutov (Intland) - implementation
+ *******************************************************************************/
+package com.vectrace.MercurialEclipse.synchronize.cs;
+
+import org.eclipse.team.internal.ui.mapping.ResourceModelActionProvider;
+
+/**
+ * @author Andrei
+ */
+@SuppressWarnings("restriction")
+public class HgChangeSetActionProvider extends ResourceModelActionProvider {
+
+}
