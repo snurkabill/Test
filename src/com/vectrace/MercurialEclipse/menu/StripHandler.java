b744b6cd8272efbc21619315b70d15d6f163a2e0
hgeclipse <hgeclipse@localhost>
2009-12-22 13:49:52 +0100

    Removed unneeded (second) call to refresh the projects (refresh is already started by wizard)

--- src/com/vectrace/MercurialEclipse/menu/StripHandler.java
+++ src/com/vectrace/MercurialEclipse/menu/StripHandler.java
@@ -11,19 +11,10 @@
  *******************************************************************************/
 package com.vectrace.MercurialEclipse.menu;
 
-import java.util.Set;
-
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
-import org.eclipse.jface.window.Window;
 import org.eclipse.jface.wizard.WizardDialog;
 
-import com.vectrace.MercurialEclipse.commands.RefreshWorkspaceStatusJob;
-import com.vectrace.MercurialEclipse.team.MercurialTeamProvider;
-import com.vectrace.MercurialEclipse.team.cache.RefreshJob;
-import com.vectrace.MercurialEclipse.utils.ResourceUtils;
 import com.vectrace.MercurialEclipse.wizards.StripWizard;
 
 public class StripHandler extends SingleResourceHandler {
@@ -34,19 +25,7 @@ public class StripHandler extends SingleResourceHandler {
 		StripWizard stripWizard = new StripWizard(project);
 		WizardDialog dialog = new WizardDialog(getShell(), stripWizard);
 		dialog.setBlockOnOpen(true);
-		if (Window.OK == dialog.open()){
-			Set<IProject> projects = ResourceUtils.getProjects(MercurialTeamProvider.getHgRoot(project));
-			for (final IProject iProject : projects) {
-				RefreshWorkspaceStatusJob job = new RefreshWorkspaceStatusJob(iProject);
-				job.addJobChangeListener(new JobChangeAdapter(){
-					@Override
-					public void done(IJobChangeEvent event) {
-						new RefreshJob("Refreshing " + iProject.getName(), iProject, RefreshJob.LOCAL).schedule();
-					}
-				});
-				job.schedule();
-			}
-		}
+		dialog.open();
 	}
 
 }
