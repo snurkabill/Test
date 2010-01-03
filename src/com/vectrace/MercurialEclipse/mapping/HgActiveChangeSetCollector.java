b1afa0acfe60ac4148192da5d3495e7dbca81546
hgeclipse <hgeclipse@localhost>
2010-01-03 23:48:15 +0100

    Backed out of changeset1690:773531f3d018

---
+++ src/com/vectrace/MercurialEclipse/mapping/HgActiveChangeSetCollector.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2006 Subclipse project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Subclipse project committers - initial API and implementation
+ *     Andrei Loskutov (Intland) - bug fixes
+ ******************************************************************************/
+package com.vectrace.MercurialEclipse.mapping;
+
+import org.eclipse.team.core.subscribers.Subscriber;
+import org.eclipse.team.internal.core.subscribers.SubscriberChangeSetManager;
+
+public class HgActiveChangeSetCollector extends SubscriberChangeSetManager {
+
+	public HgActiveChangeSetCollector(Subscriber subscriber) {
+		super(subscriber);
+	}
+
+	@Override
+	protected HgActiveChangeSet doCreateSet(String name) {
+		return new HgActiveChangeSet(this, name);
+	}
+}
---
+++ src/com/vectrace/MercurialEclipse/menu/CommitMergeHandler.java
@@ -0,0 +1,96 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2008 VecTrace (Zingo Andersen) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Andrei Loskutov (Intland) - bug fixes
+ *******************************************************************************/
+package com.vectrace.MercurialEclipse.menu;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.swt.widgets.Shell;
+
+import com.vectrace.MercurialEclipse.commands.HgCommitClient;
+import com.vectrace.MercurialEclipse.dialogs.CommitDialog;
+import com.vectrace.MercurialEclipse.dialogs.MergeDialog;
+import com.vectrace.MercurialEclipse.exception.HgException;
+import com.vectrace.MercurialEclipse.team.ResourceProperties;
+
+public class CommitMergeHandler extends SingleResourceHandler {
+
+	/**
+	 * run the commit merge handler
+	 */
+	@Override
+	protected void run(IResource resource) throws HgException {
+		Assert.isNotNull(resource);
+		commitMergeWithCommitDialog(resource.getProject(), getShell());
+	}
+
+	/**
+	 * Opens the Commit dialog and commits the merge if ok is pressed.
+	 * @param resource
+	 *            the resource
+	 * @return the hg command output
+	 * @throws HgException
+	 */
+	public String commitMergeWithCommitDialog(IProject resource, Shell shell) throws HgException {
+		Assert.isNotNull(resource);
+		String result = ""; //$NON-NLS-1$
+		try {
+			CommitDialog commitDialog = new MergeDialog(shell,  resource,
+					Messages.getString("CommitMergeHandler.mergeWith") //$NON-NLS-1$
+							+ resource.getProject().getPersistentProperty(ResourceProperties.MERGING));
+
+			// open dialog and wait for ok
+			commitDialog.open();
+		} catch (CoreException e) {
+			throw new HgException(Messages.getString("CommitMergeHandler.failedToSetMergeStatus"), e); //$NON-NLS-1$
+		}
+		return result;
+	}
+
+	/**
+	 * Commits a merge with a default merge message. The commit dialog is not shown.
+	 * @param project a project to be committed.
+	 * @return the output of hg commit
+	 * @throws HgException
+	 * @throws CoreException
+	 */
+	public static String commitMerge(IProject project) throws HgException,
+			CoreException {
+		Assert.isNotNull(project);
+		return commitMerge(project, Messages.getString("CommitMergeHandler.mergeWith") //$NON-NLS-1$
+				+ project.getPersistentProperty(ResourceProperties.MERGING));
+	}
+
+	/**
+	 * Commits a merge with the given message. The commit dialog is not shown.
+	 * @param project a project to be committed, not null
+	 * @return the output of hg commit
+	 * @throws HgException
+	 * @throws CoreException
+	 */
+	public static String commitMerge(IProject project, String message)
+			throws HgException, CoreException {
+		Assert.isNotNull(project);
+		Assert.isNotNull(message);
+
+		// do hg call
+		String result = HgCommitClient.commitProject(project, null, message);
+
+		// clear merge status in Eclipse
+		project.setPersistentProperty(ResourceProperties.MERGING, null);
+		project.setSessionProperty(ResourceProperties.MERGE_COMMIT_OFFERED, null);
+
+		project.touch(null);
+		return result;
+	}
+
+}
