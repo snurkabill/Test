611f2bd01bc7723c0d78d4a97c5e9ff730cbc38f
hgeclipse <hgeclipse@localhost>
2010-01-05 00:08:00 +0100

    Merge view / workflow improvements: - changed conflict dialog from modal dialog to usual merge editor - added context menus to:  open merge editor  open default editor  mark resolve/unresolve  show history - removed ugly text actions from the toolbar.

--- src/com/vectrace/MercurialEclipse/views/MergeView.java
+++ src/com/vectrace/MercurialEclipse/views/MergeView.java
@@ -17,17 +17,21 @@ import java.util.Observer;
 import java.util.Set;
 
 import org.eclipse.compare.CompareConfiguration;
+import org.eclipse.compare.CompareUI;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -36,13 +40,19 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.team.ui.TeamUI;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
 import org.eclipse.ui.part.ViewPart;
 
 import com.vectrace.MercurialEclipse.MercurialEclipsePlugin;
@@ -58,7 +68,6 @@ import com.vectrace.MercurialEclipse.model.FlaggedAdaptable;
 import com.vectrace.MercurialEclipse.team.MercurialRevisionStorage;
 import com.vectrace.MercurialEclipse.team.ResourceProperties;
 import com.vectrace.MercurialEclipse.team.cache.MercurialStatusCache;
-import com.vectrace.MercurialEclipse.utils.CompareUtils;
 
 public class MergeView extends ViewPart implements ISelectionListener, Observer {
 
@@ -93,36 +102,8 @@ public class MergeView extends ViewPart implements ISelectionListener, Observer 
 		table.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetDefaultSelected(SelectionEvent event) {
-				try {
-					TableItem item = (TableItem) event.item;
-					FlaggedAdaptable flagged = (FlaggedAdaptable) item.getData();
-					IFile file = (IFile) flagged.getAdapter(IFile.class);
-
-					String mergeNodeId = currentProject
-							.getPersistentProperty(ResourceProperties.MERGING);
-
-					String[] parents = HgParentClient.getParentNodeIds(currentProject);
-					int ancestor = HgParentClient.findCommonAncestor(
-							currentProject.getLocation().toFile(), parents[0],
-							parents[1]);
-
-					RevisionNode mergeNode = new RevisionNode(
-							new MercurialRevisionStorage(file, mergeNodeId));
-					RevisionNode ancestorNode = new RevisionNode(
-							new MercurialRevisionStorage(file, ancestor));
-
-					HgCompareEditorInput compareInput = new HgCompareEditorInput(
-							new CompareConfiguration(), file, ancestorNode,
-							mergeNode, true);
-
-					int returnValue = CompareUtils.openCompareDialog(compareInput);
-					if (returnValue == Window.OK && markResolvedAction.isEnabled()) {
-						markResolvedAction.run();
-					}
-				} catch (Exception e) {
-					MercurialEclipsePlugin.logError(e);
-					MercurialEclipsePlugin.showError(e);
-				}
+				TableItem item = (TableItem) event.item;
+				openMergeEditor(item);
 			}
 		});
 
@@ -158,6 +139,8 @@ public class MergeView extends ViewPart implements ISelectionListener, Observer 
 			}
 		};
 		abortAction.setEnabled(false);
+		abortAction.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(
+				ISharedImages.IMG_ELCL_STOP));
 		mgr.add(abortAction);
 		markResolvedAction = new Action(Messages.getString("MergeView.markResolved")) { //$NON-NLS-1$
 			@Override
@@ -179,7 +162,6 @@ public class MergeView extends ViewPart implements ISelectionListener, Observer 
 			}
 		};
 		markResolvedAction.setEnabled(false);
-		mgr.add(markResolvedAction);
 		markUnresolvedAction = new Action(Messages.getString("MergeView.markUnresolved")) { //$NON-NLS-1$
 			@Override
 			public void run() {
@@ -200,7 +182,60 @@ public class MergeView extends ViewPart implements ISelectionListener, Observer 
 			}
 		};
 		markUnresolvedAction.setEnabled(false);
-		mgr.add(markUnresolvedAction);
+
+		final Action openMergeEditorAction = new Action("Open in Merge Editor") {
+			@Override
+			public void run() {
+				TableItem[] selection = table.getSelection();
+				if (selection != null && selection.length > 0) {
+					openMergeEditor(selection[0]);
+				}
+			}
+		};
+
+		final Action openEditorAction = new Action("Open in Default Editor") {
+			@Override
+			public void run() {
+				IFile file = getSelection();
+				if(file == null){
+					return;
+				}
+				try {
+					IDE.openEditor(getSite().getPage(), file);
+				} catch (PartInitException e) {
+					MercurialEclipsePlugin.logError(e);
+				}
+			}
+		};
+
+		final Action actionShowHistory = new Action("Show History") {
+			@Override
+			public void run() {
+				IFile file = getSelection();
+				if(file == null){
+					return;
+				}
+				TeamUI.getHistoryView().showHistoryFor(file);
+			}
+		};
+		actionShowHistory.setImageDescriptor(MercurialEclipsePlugin.getImageDescriptor("history.gif"));
+
+		// Contribute actions to popup menu
+		final MenuManager menuMgr = new MenuManager();
+		Menu menu = menuMgr.createContextMenu(table);
+		menuMgr.addMenuListener(new IMenuListener() {
+			public void menuAboutToShow(IMenuManager menuMgr1) {
+				menuMgr1.add(openMergeEditorAction);
+				menuMgr1.add(openEditorAction);
+				menuMgr1.add(new Separator());
+				menuMgr1.add(actionShowHistory);
+				menuMgr1.add(new Separator());
+				menuMgr1.add(markResolvedAction);
+				menuMgr1.add(markUnresolvedAction);
+			}
+		});
+		menuMgr.setRemoveAllWhenShown(true);
+		table.setMenu(menu);
 	}
 
 	private void populateView(boolean attemptToCommit) throws HgException {
@@ -335,14 +370,10 @@ public class MergeView extends ViewPart implements ISelectionListener, Observer 
 		super.dispose();
 	}
 
-	/**
-	 * @return
-	 */
 	private IFile getSelection() {
 		TableItem[] selection = table.getSelection();
 		if (selection != null && selection.length > 0) {
-			FlaggedAdaptable fa = (FlaggedAdaptable) table.getSelection()[0]
-					.getData();
+			FlaggedAdaptable fa = (FlaggedAdaptable) selection[0].getData();
 			IFile iFile = ((IFile) fa.getAdapter(IFile.class));
 			return iFile;
 		}
@@ -364,6 +395,35 @@ public class MergeView extends ViewPart implements ISelectionListener, Observer 
 					setCurrentProject(backup);
 				}
 			});
+		}
+	}
+
+	private void openMergeEditor(TableItem item) {
+		try {
+			FlaggedAdaptable flagged = (FlaggedAdaptable) item.getData();
+			IFile file = (IFile) flagged.getAdapter(IFile.class);
+
+			String mergeNodeId = currentProject
+					.getPersistentProperty(ResourceProperties.MERGING);
+
+			String[] parents = HgParentClient.getParentNodeIds(currentProject);
+			int ancestor = HgParentClient.findCommonAncestor(
+					currentProject.getLocation().toFile(), parents[0],
+					parents[1]);
+
+			RevisionNode mergeNode = new RevisionNode(
+					new MercurialRevisionStorage(file, mergeNodeId));
+			RevisionNode ancestorNode = new RevisionNode(
+					new MercurialRevisionStorage(file, ancestor));
+
+			HgCompareEditorInput compareInput = new HgCompareEditorInput(
+					new CompareConfiguration(), file, ancestorNode,
+					mergeNode, true);
+
+			CompareUI.openCompareEditor(compareInput);
+		} catch (CoreException e) {
+			MercurialEclipsePlugin.logError(e);
+			MercurialEclipsePlugin.showError(e);
 		}
 	}
 
