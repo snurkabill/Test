6573e599a0adb8ed28a2d3c318f347ac6093f676
hgeclipse <hgeclipse@localhost>
2010-03-26 22:13:16 +0100

    Fix for issue #11123 - stings are empty if they contain white space only. So do not allow commit if the message contains only CR's or tabs or spaces

---
+++ src/com/vectrace/MercurialEclipse/team/cache/ResourceDeltaVisitor.java
@@ -0,0 +1,180 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2009 VecTrace (Zingo Andersen) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Andrei Loskutov (Intland) - implementation
+ *******************************************************************************/
+package com.vectrace.MercurialEclipse.team.cache;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IResourceDeltaVisitor;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.team.core.RepositoryProvider;
+import org.eclipse.ui.IWorkbenchWindow;
+
+import com.vectrace.MercurialEclipse.MercurialEclipsePlugin;
+import com.vectrace.MercurialEclipse.SafeWorkspaceJob;
+import com.vectrace.MercurialEclipse.commands.HgClients;
+import com.vectrace.MercurialEclipse.model.HgRoot;
+import com.vectrace.MercurialEclipse.operations.InitOperation;
+import com.vectrace.MercurialEclipse.preferences.MercurialPreferenceConstants;
+import com.vectrace.MercurialEclipse.team.MercurialTeamProvider;
+import com.vectrace.MercurialEclipse.team.MercurialUtilities;
+import com.vectrace.MercurialEclipse.utils.ResourceUtils;
+
+final class ResourceDeltaVisitor implements IResourceDeltaVisitor {
+
+	private final Map<IProject, Set<IResource>> removed;
+	private final Map<IProject, Set<IResource>> changed;
+	private final Map<IProject, Set<IResource>> added;
+	private final boolean autoShare;
+	private final MercurialStatusCache cache;
+	private int resourcesCount;
+
+	ResourceDeltaVisitor(Map<IProject, Set<IResource>> removed, Map<IProject, Set<IResource>> changed,
+			Map<IProject, Set<IResource>> added) {
+		this.removed = removed;
+		this.changed = changed;
+		this.added = added;
+		cache = MercurialStatusCache.getInstance();
+		autoShare = Boolean.valueOf(
+				HgClients.getPreference(MercurialPreferenceConstants.PREF_AUTO_SHARE_PROJECTS, "false"))
+				.booleanValue();
+	}
+
+	public boolean visit(IResourceDelta delta) throws CoreException {
+		IResource res = delta.getResource();
+		if (res.getType() == IResource.ROOT) {
+			return true;
+		}
+		final IProject project = res.getProject();
+
+		// handle projects that contain a mercurial repository
+		boolean openOrClosedOrDeleted = delta.getFlags() == IResourceDelta.OPEN
+			|| delta.getKind() == IResourceDelta.REMOVED;
+
+		if (autoShare && openOrClosedOrDeleted && project.isAccessible()
+				&& RepositoryProvider.getProvider(project) == null) {
+			autoshareProject(project);
+			// stop tracking changes: after auto-share is completed, we will do a full refresh anyway
+			return false;
+		}
+
+		if (!MercurialUtilities.isPossiblySupervised(res)) {
+			return false;
+		}
+		if((res == project && openOrClosedOrDeleted) || isCompleteStatusRequested()){
+			addResource(changed, project, project);
+			return false;
+		}
+		// System.out.println("Observing change on: " + res);
+
+		// NB: the resource may not exist at this point (deleted/moved)
+		// so any access to the IResource's API should be checked against null
+		if (res.getType() == IResource.FILE) {
+			IResource resource = isCompleteStatusRequested()? project : res;
+			switch (delta.getKind()) {
+			case IResourceDelta.ADDED:
+				addResource(added, project, resource);
+				// System.out.println("\t ADDED: " + resource);
+				if(isCompleteStatusRequested()){
+					return false;
+				}
+				break;
+			case IResourceDelta.CHANGED:
+				if (hasChangedBits(delta)
+						&& cache.isSupervised(project, ResourceUtils.getPath(res))) {
+
+					// fix for issue 10155: No status update after reverting changes on .hgignore
+					if(MercurialStatusCache.canTriggerFullCacheUpdate(resource)){
+						addResource(changed, project, project);
+						return false;
+					}
+					addResource(changed, project, resource);
+					// System.out.println("\t CHANGED: " + resource);
+					if(isCompleteStatusRequested()){
+						return false;
+					}
+				}
+				break;
+			case IResourceDelta.REMOVED:
+				if (cache.isSupervised(project, ResourceUtils.getPath(res))) {
+					addResource(removed, project, resource);
+					// System.out.println("\t REMOVED: " + resource);
+					if(isCompleteStatusRequested()){
+						return false;
+					}
+				} else {
+					// check the parent folder: if the file had "unknown" state, folder was
+					// marked as "modified". Now the file is deleted, so we should try
+					// to refresh the folder state
+					res = res.getParent();
+					if (res != null && cache.isSupervised(project, ResourceUtils.getPath(res))){
+						resource = isCompleteStatusRequested()? project : res;
+						addResource(changed, project, resource);
+						// System.out.println("\t CHANGED: " + resource);
+						if(isCompleteStatusRequested()){
+							return false;
+						}
+					}
+				}
+				break;
+			}
+		}
+		return true;
+	}
+
+	private boolean isCompleteStatusRequested() {
+		return resourcesCount > MercurialStatusCache.NUM_CHANGED_FOR_COMPLETE_STATUS;
+	}
+
+
+	private void addResource(Map<IProject, Set<IResource>> map, IProject project, IResource res){
+		Set<IResource> set = map.get(project);
+		if(set == null) {
+			set = new HashSet<IResource>();
+			map.put(project, set);
+		}
+		set.add(res);
+		resourcesCount ++;
+	}
+
+	private boolean hasChangedBits(IResourceDelta delta){
+		return (delta.getFlags() & MercurialStatusCache.MASK_CHANGED) != 0;
+	}
+
+	private void autoshareProject(final IProject project) {
+		final HgRoot hgRoot = MercurialTeamProvider.hasHgRoot(project);
+		if(hgRoot == null){
+			return;
+		}
+		MercurialEclipsePlugin.logInfo("Autosharing " + project.getName()
+				+ ". Detected repository location: " + hgRoot, null);
+		final IWorkbenchWindow activeWorkbenchWindow = MercurialEclipsePlugin.getActiveWindow();
+
+		new SafeWorkspaceJob(NLS.bind(Messages.mercurialStatusCache_autoshare, project.getName())) {
+			@Override
+			protected IStatus runSafe(IProgressMonitor monitor) {
+				try {
+					new InitOperation(activeWorkbenchWindow, project, hgRoot).run(monitor);
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+				return super.runSafe(monitor);
+			}
+		}.schedule();
+	}
+}
---
+++ src/com/vectrace/MercurialEclipse/ui/TagTable.java
@@ -0,0 +1,197 @@
+/*******************************************************************************
+ * Copyright (c) 2010 VecTrace (Zingo Andersen) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Jerome Negre              - implementation
+ *     Andrei Loskutov (Intland) - bug fixes
+ *     Zsolt Koppany (Intland)   - bug fixes
+ *     Philip Graf               - bug fix
+ *******************************************************************************/
+package com.vectrace.MercurialEclipse.ui;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+
+import com.vectrace.MercurialEclipse.HgRevision;
+import com.vectrace.MercurialEclipse.MercurialEclipsePlugin;
+import com.vectrace.MercurialEclipse.exception.HgException;
+import com.vectrace.MercurialEclipse.model.ChangeSet;
+import com.vectrace.MercurialEclipse.model.HgRoot;
+import com.vectrace.MercurialEclipse.model.Tag;
+import com.vectrace.MercurialEclipse.team.cache.LocalChangesetCache;
+
+/**
+ * @author Jerome Negre <jerome+hg@jnegre.org>
+ * @author <a href="mailto:zsolt.koppany@intland.com">Zsolt Koppany</a>
+ */
+public class TagTable extends Composite {
+	private final static Font PARENT_FONT = JFaceResources.getFontRegistry().getBold(JFaceResources.DIALOG_FONT);
+
+	private final Table table;
+	private int[] parents;
+	private boolean showTip;
+
+	private HgRoot hgRoot;
+
+	public TagTable(Composite parent, HgRoot hgRoot) {
+		super(parent, SWT.NONE);
+		showTip = true;
+		this.hgRoot = hgRoot;
+
+		this.setLayout(new GridLayout());
+		this.setLayoutData(new GridData());
+
+		table = new Table(this, SWT.SINGLE | SWT.BORDER | SWT.FULL_SELECTION | SWT.V_SCROLL | SWT.H_SCROLL);
+		table.setLinesVisible(true);
+		table.setHeaderVisible(true);
+		GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
+		table.setLayoutData(data);
+
+		String[] titles = { Messages.getString("TagTable.column.rev"), Messages.getString("TagTable.column.global"), Messages.getString("TagTable.column.tag"), Messages.getString("TagTable.column.local"), Messages.getString("ChangesetTable.column.summary") }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
+		int[] widths = { 60, 150, 200, 70, 300};
+		for (int i = 0; i < titles.length; i++) {
+			TableColumn column = new TableColumn(table, SWT.NONE);
+			column.setText(titles[i]);
+			column.setWidth(widths[i]);
+		}
+	}
+
+	public void hideTip() {
+		this.showTip = false;
+	}
+
+	public void highlightParents(int[] newParents) {
+		this.parents = newParents;
+	}
+
+	public void setHgRoot(HgRoot newRoot) {
+		this.hgRoot = newRoot;
+		table.removeAll();
+	}
+
+	public void setTags(Tag[] tags) {
+		table.removeAll();
+
+		for (Tag tag : tags) {
+			if (showTip || !HgRevision.TIP.getChangeset().equals(tag.getName())) {
+				TableItem row = new TableItem(table, SWT.NONE);
+				if (parents != null && isParent(tag.getRevision())) {
+					row.setFont(PARENT_FONT);
+				}
+				row.setText(0, Integer.toString(tag.getRevision()));
+				row.setText(1, tag.getGlobalId());
+				row.setText(2, tag.getName());
+				row.setText(3, tag.isLocal() ? Messages.getString("TagTable.stateLocal")  //$NON-NLS-1$
+						: Messages.getString("TagTable.stateGlobal")); //$NON-NLS-1$
+				row.setData(tag);
+			}
+		}
+		fetchChangesetInfo(tags);
+	}
+
+	void fetchChangesetInfo(final Tag[] tags) {
+		final LocalChangesetCache cache = LocalChangesetCache.getInstance();
+		Job fetchJob = new Job("Fetching hg changesets info") {
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				// this can cause UI hang for big projects. Should be done in a job.
+				// the only reason we need this is to show the changeset comments, so we can complete
+				// this data in background
+				Map<String, ChangeSet> tagged = new HashMap<String, ChangeSet>();
+				try {
+					Set<ChangeSet> allLocalRevisions = cache.refreshAllLocalRevisions(hgRoot, false, false);
+					for (ChangeSet cs : allLocalRevisions) {
+						if(monitor.isCanceled()) {
+							return Status.CANCEL_STATUS;
+						}
+						for (Tag tag : cs.getTags()) {
+							tagged.put(tag.getName(), cs);
+						}
+					}
+				} catch (HgException e1) {
+					MercurialEclipsePlugin.logError(e1);
+				}
+				final Map<Tag, ChangeSet> tagToCs = new HashMap<Tag, ChangeSet>();
+				for (Tag tag : tags) {
+					if(monitor.isCanceled()) {
+						return Status.CANCEL_STATUS;
+					}
+					if (showTip || !tag.isTip()) {
+						ChangeSet changeSet = tagged.get(tag.getName());
+						if(changeSet != null) {
+							tagToCs.put(tag, changeSet);
+						}
+					}
+				}
+
+				Runnable updateTable = new Runnable() {
+					public void run() {
+						if(table.isDisposed()) {
+							return;
+						}
+						TableItem[] items = table.getItems();
+						for (TableItem item : items) {
+							Object tag = item.getData();
+							if (tag instanceof Tag && tagToCs.get(tag) != null) {
+								item.setText(4, tagToCs.get(tag).getSummary());
+							}
+						}
+					}
+				};
+				MercurialEclipsePlugin.getStandardDisplay().asyncExec(updateTable);
+				return Status.OK_STATUS;
+			}
+		};
+		fetchJob.schedule();
+	}
+
+	public Tag getSelection() {
+		TableItem[] selection = table.getSelection();
+		if (selection.length == 0) {
+			return null;
+		}
+		return (Tag) selection[0].getData();
+	}
+
+	public void addSelectionListener(SelectionListener listener) {
+		table.addSelectionListener(listener);
+	}
+
+	private boolean isParent(int r) {
+		switch (parents.length) {
+		case 2:
+			if (r == parents[1]) {
+				return true;
+			}
+			//$FALL-THROUGH$
+		case 1:
+			if (r == parents[0]) {
+				return true;
+			}
+			//$FALL-THROUGH$
+		default:
+			return false;
+		}
+	}
+
+}
