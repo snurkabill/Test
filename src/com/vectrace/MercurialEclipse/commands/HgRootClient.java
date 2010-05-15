51b26adffbb83d53adf32fa8b57895a4847b0b15
npiguet <npiguet@localhost>
2010-05-15 13:58:25 +0200

    Reverted HgRootClient to 2071:402d607a786d

--- src/com/vectrace/MercurialEclipse/commands/HgRootClient.java
+++ src/com/vectrace/MercurialEclipse/commands/HgRootClient.java
@@ -37,15 +37,30 @@ public class HgRootClient extends AbstractClient {
 	 * @throws HgException
 	 */
 	public static HgRoot getHgRoot(File file) throws HgException {
-		File dir = ResourceUtils.getFirstExistingDirectory(file);
+		// HgCommand command = new HgCommand("root", proj, true);
+		// return new String(command.executeToBytes(Integer.MAX_VALUE)).replaceAll("\n", "");
 
-		HgRoot hgRoot = getHgRootFromCache(dir);
+		File dir = ResourceUtils.getFirstExistingDirectory(file);
+		// test if we have the path "as is" already
+		HgRoot hgRoot = ROOTS.get(dir);
 		if(hgRoot != null){
 			return hgRoot;
 		}
-
-		// search up the parents recursive (with canonical file) if we see .hg directory there
-		File root = findHgDir(getCanonicalPath(dir));
+		HgRoot testRoot;
+		try {
+			testRoot = new HgRoot(dir);
+		} catch(IOException e) {
+			throw new HgException(Messages.getString("HgRootClient.error.cannotGetCanonicalPath")+file.getName()); //$NON-NLS-1$
+		}
+		// test with canonical version of the same file
+		hgRoot = ROOTS.get(testRoot);
+		if(hgRoot != null){
+			// remember NON canonical version too
+			ROOTS.put(dir, hgRoot);
+			return hgRoot;
+		}
+		// search up the parents recursive if we see .hg directory there
+		File root = findHgDir(testRoot);
 		if (root == null) {
 			throw new HgException(file.getName() + Messages.getString("HgRootClient.error.noRoot")); //$NON-NLS-1$
 		}
@@ -57,33 +72,6 @@ public class HgRootClient extends AbstractClient {
 		}
 		ROOTS.put(root, hgRoot);
 		return hgRoot;
-	}
-
-	private static HgRoot getHgRootFromCache(File dir) throws HgException {
-		// test if we have the path "as is" already
-		HgRoot hgRoot = ROOTS.get(dir);
-		if(hgRoot != null){
-			return hgRoot;
-		}
-
-		HgRoot testRoot = getCanonicalPath(dir);
-
-		// test with canonical version of the same file
-		hgRoot = ROOTS.get(testRoot);
-		if(hgRoot != null){
-			// remember NON canonical version too
-			ROOTS.put(dir, hgRoot);
-		}
-		return hgRoot;
-	}
-
-	private static HgRoot getCanonicalPath(File dir) throws HgException {
-		// build canonical path of dir
-		try {
-			return new HgRoot(dir);
-		} catch(IOException e) {
-			throw new HgException(Messages.getString("HgRootClient.error.cannotGetCanonicalPath")+dir.getName()); //$NON-NLS-1$
-		}
 	}
 
 	/**
@@ -100,39 +88,17 @@ public class HgRootClient extends AbstractClient {
 	}
 
 	/**
-	 * Returns the HgRoot in the specified path, or null if there is not hg root in that path. This method does not recurse in parents to find an HgRoot.
-	 */
-	public static HgRoot hasHgRootDirect(File file){
-		if(!file.isDirectory()){
-			return null;
-		}
-
-		try{
-			HgRoot cached = getHgRootFromCache(file);
-			if(cached != null){
-				return cached;
-			}
-
-			HgRoot canonical = getCanonicalPath(file);
-			File[] contents = canonical.listFiles(new HgRootFileFilter());
-			if(contents.length == 0){
-				return null;
-			}
-			return canonical;
-
-		}catch(HgException hge){
-			return null;
-		}
-	}
-
-	/**
 	 * Searches the .hg directory up in all parents of the given file
 	 * @param startDir a directory
 	 * @return directory named ".hg" (in any case), or null if the given file does not
 	 * have parents wich contains .hg directory
 	 */
 	private static File findHgDir(File startDir) {
-		FileFilter hg = new HgRootFileFilter();
+		FileFilter hg = new FileFilter() {
+			public boolean accept(File path) {
+				return path.getName().equalsIgnoreCase(".hg") && path.isDirectory(); //$NON-NLS-1$
+			}
+		};
 		File root = startDir;
 		File[] rootContent = root.listFiles(hg);
 		while (rootContent != null && rootContent.length == 0) {
@@ -148,9 +114,4 @@ public class HgRootClient extends AbstractClient {
 		return null;
 	}
 
-	private static class HgRootFileFilter implements FileFilter {
-		public boolean accept(File path) {
-			return path.getName().equalsIgnoreCase(".hg") && path.isDirectory();
-		}
-	}
 }
