205acb39492190c1527c203acd9dfdd06b3cc855
hgeclipse <hgeclipse@localhost>
2010-05-22 18:19:21 +0200

    Fixed silly startup error mesages about empty repo locations read from hgrc files

--- src/com/vectrace/MercurialEclipse/commands/HgPathsClient.java
+++ src/com/vectrace/MercurialEclipse/commands/HgPathsClient.java
@@ -17,11 +17,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import com.vectrace.MercurialEclipse.exception.HgException;
 import com.vectrace.MercurialEclipse.model.Branch;
 import com.vectrace.MercurialEclipse.model.HgRoot;
 import com.vectrace.MercurialEclipse.utils.IniFile;
+import com.vectrace.MercurialEclipse.utils.StringUtils;
 
 public class HgPathsClient extends AbstractClient {
 	public static final String DEFAULT = Branch.DEFAULT;
@@ -29,6 +32,11 @@ public class HgPathsClient extends AbstractClient {
 	public static final String DEFAULT_PUSH = "default-push"; //$NON-NLS-1$
 	public static final String PATHS_SECTION = "paths"; //$NON-NLS-1$
 
+	/**
+	 * @param hgRoot non null
+	 * @return map with "logical name" : "url" pairs. May be empty.
+	 * @throws HgException
+	 */
 	public static Map<String, String> getPaths(HgRoot hgRoot) throws HgException {
 		File hgrc = hgRoot.getConfig();
 		Map<String, String> paths = new HashMap<String, String>();
@@ -39,7 +47,14 @@ public class HgPathsClient extends AbstractClient {
 			IniFile ini = new IniFile(hgrc.getAbsolutePath());
 			Map<String, String> section = ini.getSection(PATHS_SECTION);
 			if (section != null) {
-				paths.putAll(section);
+				Set<Entry<String,String>> entrySet = section.entrySet();
+				for (Entry<String, String> entry : entrySet) {
+					String logicalName = entry.getKey();
+					String path = entry.getValue();
+					if(!StringUtils.isEmpty(logicalName) && !StringUtils.isEmpty(path)) {
+						paths.put(logicalName, path);
+					}
+				}
 			}
 		} catch (IOException e) {
 			throw new HgException("Unable to read paths from: " + hgrc, e);
