e1c96271e8285f2ddb6fc4b4410bb006708d4603
zkoppany <zkoppany@localhost>
2010-08-11 11:21:00 +0200

    #12515 The plugin needs hg-1.5.1 or higher.

---
+++ src/com/vectrace/MercurialEclipse/commands/HgResolveClient.java
@@ -0,0 +1,175 @@
+/*******************************************************************************
+ * Copyright (c) 2008 VecTrace (Zingo Andersen) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Bastian Doetsch           - implementation
+ *     Andrei Loskutov (Intland) - bug fixes
+ *     Adam Berkes (Intland)     - bug fixes
+ *******************************************************************************/
+package com.vectrace.MercurialEclipse.commands;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Path;
+
+import com.vectrace.MercurialEclipse.MercurialEclipsePlugin;
+import com.vectrace.MercurialEclipse.exception.HgException;
+import com.vectrace.MercurialEclipse.model.FlaggedAdaptable;
+import com.vectrace.MercurialEclipse.model.HgRoot;
+import com.vectrace.MercurialEclipse.preferences.MercurialPreferenceConstants;
+import com.vectrace.MercurialEclipse.team.cache.MercurialStatusCache;
+import com.vectrace.MercurialEclipse.utils.ResourceUtils;
+
+public class HgResolveClient extends AbstractClient {
+
+	/**
+	 * List merge state of files after merge
+	 */
+	public static List<FlaggedAdaptable> list(IResource res) throws HgException {
+		AbstractShellCommand command = new HgCommand("resolve", getWorkingDirectory(res), //$NON-NLS-1$
+				false);
+		command
+				.setUsePreferenceTimeout(MercurialPreferenceConstants.IMERGE_TIMEOUT);
+		command.addOptions("-l"); //$NON-NLS-1$
+		String[] lines = command.executeToString().split("\n"); //$NON-NLS-1$
+		List<FlaggedAdaptable> result = new ArrayList<FlaggedAdaptable>();
+		if (lines.length != 1 || !"".equals(lines[0])) { //$NON-NLS-1$
+			HgRoot hgRoot = getHgRoot(res);
+			IProject project = res.getProject();
+			for (String line : lines) {
+				// Status line is always hg root relative. For those projects
+				// which has different project root (always deeper than hg root)
+				// hg root relative path must be converted
+				IResource iFile = ResourceUtils.convertRepoRelPath(hgRoot, project, line.substring(2));
+				if(iFile != null){
+					FlaggedAdaptable fa = new FlaggedAdaptable(iFile, line.charAt(0));
+					result.add(fa);
+				}
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * List merge state of files after merge
+	 */
+	public static List<FlaggedAdaptable> list(HgRoot hgRoot) throws HgException {
+		AbstractShellCommand command = new HgCommand("resolve", hgRoot, false);
+		command.setUsePreferenceTimeout(MercurialPreferenceConstants.IMERGE_TIMEOUT);
+		command.addOptions("-l"); //$NON-NLS-1$
+		String[] lines = command.executeToString().split("\n"); //$NON-NLS-1$
+		List<FlaggedAdaptable> result = new ArrayList<FlaggedAdaptable>();
+		if (lines.length != 1 || !"".equals(lines[0])) { //$NON-NLS-1$
+			for (String line : lines) {
+				// Status line is always hg root relative. For those projects
+				// which has different project root (always deeper than hg root)
+				// hg root relative path must be converted
+				String repoRelPath = line.substring(2);
+				IResource iFile = ResourceUtils.getFileHandle(hgRoot.toAbsolute(new Path(repoRelPath)));
+				if(iFile != null){
+					FlaggedAdaptable fa = new FlaggedAdaptable(iFile, line.charAt(0));
+					result.add(fa);
+				}
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Mark a resource as resolved ("R")
+	 */
+	public static String markResolved(IFile ifile) throws HgException {
+		File file = ResourceUtils.getFileHandle(ifile);
+		HgCommand command = new HgCommand("resolve", //$NON-NLS-1$
+				getWorkingDirectory(file), false);
+		command.setExecutionRule(new AbstractShellCommand.ExclusiveExecutionRule(command
+				.getHgRoot()));
+		command.setUsePreferenceTimeout(MercurialPreferenceConstants.IMERGE_TIMEOUT);
+			try {
+			command.addOptions("-m", file.getCanonicalPath()); //$NON-NLS-1$
+			String result = command.executeToString();
+			// cleanup .orig files left after merge
+			File origFile = new File(file.getAbsolutePath() + ".orig");
+			if(origFile.isFile()){
+				IResource fileToDelete = ResourceUtils.convert(origFile);
+				boolean deleted = origFile.delete();
+				if(!deleted){
+					MercurialEclipsePlugin.logInfo("Failed to cleanup " + origFile + " file after merge", null);
+				} else {
+					try {
+						fileToDelete.refreshLocal(IResource.DEPTH_ZERO, null);
+					} catch (CoreException e) {
+						MercurialEclipsePlugin.logError(e);
+					}
+				}
+			}
+			refreshStatus(ifile);
+			return result;
+		} catch (IOException e) {
+			throw new HgException(e.getLocalizedMessage(), e);
+		}
+	}
+
+	/**
+	 * Try to resolve all unresolved files
+	 */
+	public static String resolveAll(IResource res) throws HgException {
+		File file = res.getLocation().toFile();
+		HgCommand command = new HgCommand("resolve", getWorkingDirectory(file), //$NON-NLS-1$
+				false);
+		command.setExecutionRule(new AbstractShellCommand.ExclusiveExecutionRule(command.getHgRoot()));
+		boolean useExternalMergeTool = Boolean.valueOf(
+				HgClients.getPreference(
+						MercurialPreferenceConstants.PREF_USE_EXTERNAL_MERGE,
+						"false")).booleanValue(); //$NON-NLS-1$
+		if (!useExternalMergeTool) {
+			// we use an non-existent UI Merge tool, so no tool is started. We
+			// need this option, though, as we still want the Mercurial merge to
+			// take place.
+			command.addOptions("--config", "ui.merge=simplemerge"); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+		command
+				.setUsePreferenceTimeout(MercurialPreferenceConstants.IMERGE_TIMEOUT);
+		String result = command.executeToString();
+		refreshStatus(res);
+		return result;
+
+	}
+
+	/**
+	 * Mark a resource as unresolved ("U")
+	 */
+	public static String markUnresolved(IFile ifile) throws HgException {
+		File file = ifile.getLocation().toFile();
+		HgCommand command = new HgCommand("resolve", //$NON-NLS-1$
+				getWorkingDirectory(file), false);
+		command.setExecutionRule(new AbstractShellCommand.ExclusiveExecutionRule(command
+				.getHgRoot()));
+		command.setUsePreferenceTimeout(MercurialPreferenceConstants.IMERGE_TIMEOUT);
+		try {
+			command.addOptions("-u", file.getCanonicalPath()); //$NON-NLS-1$
+			String result = command.executeToString();
+			refreshStatus(ifile);
+			return result;
+		} catch (IOException e) {
+			throw new HgException(e.getLocalizedMessage(), e);
+		}
+	}
+
+	private static void refreshStatus(IResource res) throws HgException {
+		MercurialStatusCache.getInstance().refreshStatus(res, null);
+		ResourceUtils.touch(res);
+	}
+
+}
---
+++ src/com/vectrace/MercurialEclipse/history/ExclusiveHistoryRule.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2008 VecTrace (Zingo Andersen) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Andrei Loskutov (Intland) - implementation
+ *******************************************************************************/
+package com.vectrace.MercurialEclipse.history;
+
+import org.eclipse.core.runtime.jobs.ISchedulingRule;
+
+/**
+ * A rule which disallows parallel execution of jobs using it
+ */
+public final class ExclusiveHistoryRule implements ISchedulingRule {
+	public boolean isConflicting(ISchedulingRule rule) {
+		return contains(rule);
+	}
+
+	public boolean contains(ISchedulingRule rule) {
+		return rule instanceof ExclusiveHistoryRule;
+	}
+}
