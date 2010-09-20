191e4478c368f39c295d3522853267da380bc505
ilya_ivanov <ilya_ivanov@localhost>
2010-09-20 15:30:57 +0400

    #12584 Better notification when repository is not accessible

---
+++ src/com/vectrace/MercurialEclipse/commands/HgUpdateClient.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2008 VecTrace (Zingo Andersen) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Andrei Loskutov (Intland) - bug fixes
+ *******************************************************************************/
+package com.vectrace.MercurialEclipse.commands;
+
+import com.vectrace.MercurialEclipse.exception.HgException;
+import com.vectrace.MercurialEclipse.model.HgRoot;
+import com.vectrace.MercurialEclipse.preferences.MercurialPreferenceConstants;
+import com.vectrace.MercurialEclipse.team.cache.RefreshRootJob;
+import com.vectrace.MercurialEclipse.team.cache.RefreshWorkspaceStatusJob;
+
+public class HgUpdateClient extends AbstractClient {
+
+	public static void update(final HgRoot hgRoot, String revision, boolean clean)
+			throws HgException {
+		HgCommand command = new HgCommand("update", hgRoot, false); //$NON-NLS-1$
+		command.setExecutionRule(new AbstractShellCommand.ExclusiveExecutionRule(hgRoot));
+		command.setUsePreferenceTimeout(MercurialPreferenceConstants.UPDATE_TIMEOUT);
+		if (revision != null && revision.trim().length() > 0) {
+			command.addOptions("-r", revision); //$NON-NLS-1$
+		}
+		if (clean) {
+			command.addOptions("-C"); //$NON-NLS-1$
+		}
+
+		try {
+			command.executeToBytes();
+		} finally {
+			new RefreshWorkspaceStatusJob(hgRoot, RefreshRootJob.LOCAL).schedule();
+		}
+	}
+}
