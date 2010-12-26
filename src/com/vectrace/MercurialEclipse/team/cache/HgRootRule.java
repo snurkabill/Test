e03d8a4926b5641fc13236f4fe81a9dfe7f83ed7
iloveeclipse <iloveeclipse@localhost>
2010-12-26 09:31:04 +0100

    Fixed deadlock on startup, see issue #13474

--- src/com/vectrace/MercurialEclipse/team/cache/HgRootRule.java
+++ src/com/vectrace/MercurialEclipse/team/cache/HgRootRule.java
@@ -1,29 +1,38 @@
 /*******************************************************************************
- * Copyright (c) 2010 Andrei Loskutov (Intland).
+ * Copyright (c) 2010 Andrei Loskutov.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- * 		Andrei Loskutov (Intland) - implementation
+ * 		Andrei Loskutov 		- implementation
  *******************************************************************************/
 package com.vectrace.MercurialEclipse.team.cache;
 
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
+import org.eclipse.team.core.RepositoryProvider;
 
 import com.vectrace.MercurialEclipse.model.HgRoot;
 
 /**
+ * Exclusive rule for locking access to the resources related to same hg root.
+ *
  * @author Andrei
  */
 public class HgRootRule implements ISchedulingRule {
 
 	private final HgRoot hgRoot;
 
+	/**
+	 * @param hgRoot non null
+	 */
 	public HgRootRule(HgRoot hgRoot) {
+		Assert.isNotNull(hgRoot, "Trying to create HgRootRule without a hgRoot object");
 		this.hgRoot = hgRoot;
 	}
 
@@ -31,17 +40,27 @@ public class HgRootRule implements ISchedulingRule {
 		return isConflicting(rule);
 	}
 
+	/**
+	 * Note: this method (used by Job API to detect deadlocks) should avoid made locking calls to
+	 * avoid deadlocks, see issue 13474
+	 */
 	public boolean isConflicting(ISchedulingRule rule) {
 		if(!(rule instanceof HgRootRule)){
 			if(rule instanceof IWorkspaceRoot){
 				return false;
 			}
-			if(rule instanceof IResource){
-				HgRoot resourceRoot = MercurialRootCache.getInstance().hasHgRoot((IResource) rule);
-				if(resourceRoot == null) {
-					return false;
+			if (rule instanceof IResource) {
+				IProject project = ((IResource) rule).getProject();
+				// isShared() call does not lock. In case project is not shared, we don't care
+				if (project != null && RepositoryProvider.isShared(project)) {
+					// hasHgRoot() locks but only in case the project was unshared before the call (issue 13474)
+					// but now it's safe to use it as we know the project was already shared
+					HgRoot resourceRoot = MercurialRootCache.getInstance().hasHgRoot((IResource) rule);
+					if(resourceRoot == null) {
+						return false;
+					}
+					return getHgRoot().equals(resourceRoot);
 				}
-				return getHgRoot().equals(resourceRoot);
 			}
 			return false;
 		}
@@ -57,10 +76,8 @@ public class HgRootRule implements ISchedulingRule {
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		builder.append("HgRootRule [");
-		if (hgRoot != null) {
-			builder.append("hgRoot=");
-			builder.append(hgRoot);
-		}
+		builder.append("hgRoot=");
+		builder.append(hgRoot);
 		builder.append("]");
 		return builder.toString();
 	}
