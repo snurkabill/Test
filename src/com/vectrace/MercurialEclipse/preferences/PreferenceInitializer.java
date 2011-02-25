7baed93b5064f0464b6de564e8846059403285fa
John Peberdy <john@peberdy.ca>
2011-02-25 01:08:02 -0500

    #13549: History view executes inneficient query sometimes: disable show all tags by default

--- src/com/vectrace/MercurialEclipse/preferences/PreferenceInitializer.java
+++ src/com/vectrace/MercurialEclipse/preferences/PreferenceInitializer.java
@@ -92,7 +92,7 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 		store.setDefault(PREF_SHOW_COMMENTS, true);
 		store.setDefault(PREF_SHOW_PATHS, true);
 		store.setDefault(PREF_SHOW_DIFFS, true);
-		store.setDefault(PREF_SHOW_ALL_TAGS, true);
+		store.setDefault(PREF_SHOW_ALL_TAGS, false);
 		store.setDefault(PREF_SHOW_GOTO_TEXT, true);
 		store.setDefault(PREF_AFFECTED_PATHS_LAYOUT, LAYOUT_HORIZONTAL);
 		store.setDefault(PREF_SIGCHECK_IN_HISTORY, false);
