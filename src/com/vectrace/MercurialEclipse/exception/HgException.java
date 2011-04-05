1a7342c25dc87111916db9a07d545c36ceb1afdc
iloveeclipse <iloveeclipse@localhost>
2011-04-05 22:30:52 +0200

    Record stack trace (throwable in status is used for getRootCause()) in HgException

--- src/com/vectrace/MercurialEclipse/exception/HgException.java
+++ src/com/vectrace/MercurialEclipse/exception/HgException.java
@@ -7,7 +7,7 @@
  *
  * Contributors:
  *     Software Balm Consulting Inc (Peter Hunnisett <peter_hge at softwarebalm dot com>) - implementation
- *     Andrei Loskutov (Intland) - bug fixes
+ *     Andrei Loskutov - bug fixes
  *******************************************************************************/
 package com.vectrace.MercurialEclipse.exception;
 
@@ -39,7 +39,7 @@ public class HgException extends TeamException {
 
 	public HgException(String message) {
 		super(new Status(IStatus.ERROR, MercurialEclipsePlugin.ID,
-				OPERATION_FAILED, message, null));
+				OPERATION_FAILED, message, new Exception(message)));
 	}
 
 	public HgException(CoreException e) {
