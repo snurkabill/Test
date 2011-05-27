fb11bafcde32f7bafeef44598f436473fbbd705f
iloveeclipse <iloveeclipse@localhost>
2011-05-27 23:14:50 +0200

    Fix for incompatibility with Eclipse 4.1 (see issue #17064)

--- src/com/vectrace/MercurialEclipse/menu/RootHandler.java
+++ src/com/vectrace/MercurialEclipse/menu/RootHandler.java
@@ -1,27 +1,27 @@
 /*******************************************************************************
- * Copyright (c) 2010 Andrei Loskutov (Intland).
+ * Copyright (c) 2010 Andrei Loskutov.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Andrei Loskutov (Intland) - implementation
+ *     Andrei Loskutov - implementation
  *******************************************************************************/
 package com.vectrace.MercurialEclipse.menu;
-
-import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.expressions.EvaluationContext;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 import com.vectrace.MercurialEclipse.MercurialEclipsePlugin;
 import com.vectrace.MercurialEclipse.exception.HgException;
@@ -45,22 +45,19 @@ public abstract class RootHandler extends AbstractHandler {
 		return selection;
 	}
 
-	@SuppressWarnings("unchecked")
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		Object selectionObject = ((EvaluationContext) event
-				.getApplicationContext()).getDefaultVariable();
+		ISelection selectionObject = HandlerUtil.getCurrentSelection(event);
 		try {
-			if (selectionObject != null && selectionObject instanceof List) {
-				List list = (List) selectionObject;
-				Object listEntry = list.get(0);
-				if (listEntry != null && listEntry instanceof IAdaptable) {
+			if (selectionObject != null && selectionObject instanceof IStructuredSelection) {
+				Object listEntry = ((IStructuredSelection)selectionObject).getFirstElement();
+				if (listEntry instanceof IAdaptable) {
 					IAdaptable adaptable = (IAdaptable) listEntry;
 					selection = MercurialEclipsePlugin.getAdapter(adaptable, HgRoot.class);
-					if(selection == null){
-						IResource resource = MercurialEclipsePlugin.getAdapter(adaptable, IResource.class);
-						if(resource != null){
-							selection = MercurialTeamProvider.getHgRoot(resource);
-						}
+				}
+				if(selection == null){
+					IResource resource = ResourceUtils.getResource(listEntry);
+					if(resource != null){
+						selection = MercurialTeamProvider.getHgRoot(resource);
 					}
 				}
 			}
