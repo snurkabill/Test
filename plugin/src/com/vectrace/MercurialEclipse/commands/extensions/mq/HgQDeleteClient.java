3c1b8f32766bce6e67743472ef095f44626fd979
John Peberdy <john@peberdy.ca>
2012-07-06 01:24:48 -0400

    Use JavaHg for QDelete

--- plugin/src/com/vectrace/MercurialEclipse/commands/extensions/mq/HgQDeleteClient.java
+++ plugin/src/com/vectrace/MercurialEclipse/commands/extensions/mq/HgQDeleteClient.java
@@ -11,14 +11,16 @@
 package com.vectrace.MercurialEclipse.commands.extensions.mq;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.core.runtime.Assert;
 
+import com.aragost.javahg.commands.ExecutionException;
 import com.aragost.javahg.ext.mq.Patch;
+import com.aragost.javahg.ext.mq.QDeleteCommand;
+import com.aragost.javahg.ext.mq.flags.QDeleteCommandFlags;
 import com.vectrace.MercurialEclipse.commands.AbstractClient;
-import com.vectrace.MercurialEclipse.commands.AbstractShellCommand;
-import com.vectrace.MercurialEclipse.commands.HgCommand;
 import com.vectrace.MercurialEclipse.exception.HgException;
 import com.vectrace.MercurialEclipse.model.ChangeSet;
 import com.vectrace.MercurialEclipse.model.HgRoot;
@@ -28,42 +30,41 @@ import com.vectrace.MercurialEclipse.model.HgRoot;
  *
  */
 public class HgQDeleteClient extends AbstractClient {
-	public static String delete(HgRoot root, boolean keep, String patch) throws HgException {
+	public static void delete(HgRoot root, boolean keep, String patch) throws HgException {
 		List<String> patches = new ArrayList<String>(1);
 		patches.add(patch);
-		return doDelete(root, keep, null, patches);
+		doDelete(root, keep, null, patches);
 	}
 
-	public static String delete(HgRoot root, boolean keep,
-			ChangeSet changeset, List<Patch> patches) throws HgException {
+	public static void delete(HgRoot root, boolean keep, ChangeSet changeset, List<Patch> patches)
+			throws HgException {
 		Assert.isNotNull(patches);
 
 		List<String> patcheNames = new ArrayList<String>(patches.size());
 		for (Patch patch : patches) {
 			patcheNames.add(patch.getName());
 		}
-		return doDelete(root, keep, changeset, patcheNames);
+		doDelete(root, keep, changeset, patcheNames);
 	}
 
-	private static String doDelete(HgRoot root, boolean keep,
-			ChangeSet changeset, List<String> patches) throws HgException {
+	private static void doDelete(HgRoot root, boolean keep, ChangeSet changeset,
+			List<String> patches) throws HgException {
+		QDeleteCommand command = QDeleteCommandFlags.on(root.getRepository());
+
 		Assert.isNotNull(patches);
-		Assert.isNotNull(root);
-		AbstractShellCommand command = new HgCommand("qdelete", //$NON-NLS-1$
-				"Invoking qdelete", root, true);
-		command.setExecutionRule(new AbstractShellCommand.ExclusiveExecutionRule(root));
-		command.addOptions("--config", "extensions.hgext.mq="); //$NON-NLS-1$ //$NON-NLS-2$
 
 		if (keep) {
-			command.addOptions("--keep"); //$NON-NLS-1$
+			command.keep();
 		}
 		if (changeset != null) {
-			command.addOptions("--rev", changeset.getNode()); //$NON-NLS-1$
-		} else {
-			for (String patch : patches) {
-				command.addOptions(patch);
-			}
+			command.rev(changeset.getNode());
+			patches = Collections.EMPTY_LIST;
 		}
-		return command.executeToString();
+
+		try {
+			command.execute(patches.toArray(new String[patches.size()]));
+		} catch (ExecutionException ex) {
+			throw new HgException(ex.getLocalizedMessage(), ex);
+		}
 	}
 }
