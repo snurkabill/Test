a9808f815a9da081058f8bcc62b2be042f85fad5
Amenel Voglozin <amenel@khalemy.com>
2016-06-23 09:46:29 +0200

    Fixed #417 ("Commit dialog ignores file selection").

--- plugin/src/com/vectrace/MercurialEclipse/team/ActionCommit.java
+++ plugin/src/com/vectrace/MercurialEclipse/team/ActionCommit.java
@@ -6,22 +6,31 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     David Watson - implementation
+ *     David Watson 		- implementation
+ *     Amenel VOGLOZIN 		- (2016-06-22) Only selected files (or the current editor file) will be listed in the dialog
  *******************************************************************************/
 package com.vectrace.MercurialEclipse.team;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
 import com.vectrace.MercurialEclipse.MercurialEclipsePlugin;
 import com.vectrace.MercurialEclipse.exception.HgException;
 import com.vectrace.MercurialEclipse.menu.CommitHandler;
+import com.vectrace.MercurialEclipse.team.cache.MercurialStatusCache;
 
 public class ActionCommit extends ActionDelegate {
 
 	/**
-	 * The action has been activated. The argument of the method represents the
-	 * 'real' action sitting in the workbench UI.
+	 * The action has been activated. The argument of the method represents the 'real' action
+	 * sitting in the workbench UI.
+	 *
 	 * @throws HgException
 	 *
 	 * @see IWorkbenchWindowActionDelegate#run
@@ -29,10 +38,50 @@ public class ActionCommit extends ActionDelegate {
 	@Override
 	public void run(IAction action) {
 		try {
-			new CommitHandler().run(getSelectedHgProjects());
+			List<IResource> resources = new ArrayList<IResource>();
+			boolean merging = collectResourcesToCommit(resources);
+			if (!merging) {
+				if (resources.size() == 0) {
+					resources = getSelectedHgProjects();
+				}
+				doRun(resources);
+			} else {
+				Shell shell = MercurialEclipsePlugin.getActiveShell();
+				boolean doCommit = MessageDialog.openConfirm(shell,
+						Messages.getString("ActionCommit.HgCommit"), //$NON-NLS-1$
+						Messages.getString("ActionCommit.mergeIsRunning")); //$NON-NLS-1$
+				if (doCommit) {
+					doRun(resources);
+				}
+			}
 		} catch (HgException e) {
 			MercurialEclipsePlugin.logError(e);
 			MercurialEclipsePlugin.showError(e);
 		}
 	}
+
+	private static void doRun(List<IResource> resources) throws HgException {
+		new CommitHandler().run(resources);
+	}
+
+	// @Amenel: Inspired from ActionRevert. Given that both Revert and Commit operate on modified
+	// files, I've chosen to maintain the merge flag.
+	private boolean collectResourcesToCommit(List<IResource> resources) {
+		boolean mergeIsRunning = false;
+		for (Object obj : selection.toList()) {
+			if (obj instanceof IResource) {
+				IResource resource = (IResource) obj;
+				boolean merging = MercurialStatusCache.getInstance().isMergeInProgress(resource);
+				if (merging) {
+					mergeIsRunning = true;
+				}
+				boolean supervised = MercurialTeamProvider.isHgTeamProviderFor(resource);
+				if (supervised) {
+					resources.add(resource);
+				}
+			}
+		}
+		return mergeIsRunning;
+	}
+
 }
